265dadcb954119e01b49030e83a9b6e0a0cec6fd
Rosen <onehundredandfourtytwo@gmail.com>
2018-04-01 14:28:27 +0300

    Removed duplicate dir

--- RosenW-googlemaps/src/main/java/map/repository/MarkerRepository.java
+++
@@ -1,9 +0,0 @@
-package map.repository;
-
-import map.entities.Marker;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MarkerRepository extends JpaRepository<Marker, Long> {
-}
