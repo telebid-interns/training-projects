5c12dba1d68b7259fac1d8f9c4ac55e995dccb3f
rosen <onehundredandfourtytwo@gmail.com>
2018-06-07 17:44:38 +0300

    made a request dispatcher and routing?

--- RosenW-WebServer/server.py
+++ RosenW-WebServer/server.py
@@ -1,28 +1,103 @@
 import socket
 import codecs
 
-HOST, PORT = '', 8888
+class Server:
+    address_family = socket.AF_INET
+    socket_type = socket.SOCK_STREAM
+    request_queue_size = 1
 
-listen_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-listen_socket.bind((HOST, PORT))
-listen_socket.listen(1)
-print 'Serving HTTP on port %s ...' % PORT
-while True:
-    client_connection, client_address = listen_socket.accept()
-    request = client_connection.recv(1024)
+    def __init__(self, server_address):
+        self.listen_socket = listen_socket = socket.socket(self.address_family, self.socket_type)
+        listen_socket.bind(server_address)
+        listen_socket.listen(self.request_queue_size)
 
-    requestTokens = request.split();
-    # print requestTokens
+    def start(self):
+        print 'Server Started'
+        listen_socket = self.listen_socket
+        while True:
+            # New client connection
+            self.client_connection, client_address = listen_socket.accept()
+            # Handle one request and close the client connection. Then
+            # loop over to wait for another client connection
+            self.handle_request()
 
-    method = requestTokens[0];
-    if method == 'GET':
-        print 'get method'
-    elif method == 'POST':
-        print 'post method'
+    def handle_request(self):
+        #receive request data
+        self.request = request = self.client_connection.recv(1024)
+        # Print formatted request data a la 'curl -v'
+        print(''.join(
+            '< {line}\n'.format(line=line)
+            for line in request.splitlines()
+        ))
 
-    htmlf=codecs.open("./views/hello.html", 'r', 'utf-8')
-    print
+        self.parse_request(request)
 
-    http_response = "HTTP/1.1 200 OK\n\n" + htmlf.read();
-    client_connection.sendall(http_response)
-    client_connection.close()
+        # Construct a response and send it back to the client
+        self.send_response()
+
+    def parse_request(self, request):
+        line = request.splitlines()[0]
+        line = line.rstrip('\r\n')
+        # Break down the request line into components
+        (self.request_method,  # GET
+         self.path,            # /hello
+         self.request_version  # HTTP/1.1
+         ) = line.split()
+
+    def send_response(self):
+        try:
+            response = self.dispatch_request(self.path, self.request_method)
+            self.client_connection.sendall(response)
+        except Exception as e:
+            print e
+        finally:
+            self.client_connection.close()
+
+    def dispatch_request(self, path, method):
+        if self.request_method == 'GET':
+            return self.call_get_function(path)
+        else:
+            return self.call_post_function(path)
+
+    def call_get_function(self, path):
+        try:
+            print path
+            func = {
+                '/sum': self.get_sum,
+                '/file': self.get_file
+            }.get(path)
+            return func()
+        except Exception as e:
+            print 'No such route'
+
+    def call_post_function(self, path):
+        try:
+            print path
+            func = {
+                '/file': self.post_file
+            }.get(path)
+            return func()
+        except Exception as e:
+            print 'PAGE DOES NOT EXIST'
+
+    def get_page(self, path):
+        #try return error code etc
+        htmlf=codecs.open("./views/" + path + ".html", 'r', 'utf-8')
+        return "HTTP/1.1 200 OK\n\n" + htmlf.read()
+
+    def get_sum(self):
+        print 'in get sum'
+        return self.get_page('sum')
+
+    def get_file(self):
+        print 'IN GET FILE'
+        return self.get_page('file')
+
+    def post_file(self):
+        print self.request
+        print 'BEFORE GET FILE'
+        return self.get_file()
+
+if __name__ == '__main__':
+    server = Server(('', 8888))
+    server.start()
