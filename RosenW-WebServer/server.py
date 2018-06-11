efb555dcef169ef521a4569383c09de7af1a55ad
rosen <onehundredandfourtytwo@gmail.com>
2018-06-11 16:16:33 +0300

    minor changes

--- RosenW-WebServer/server.py
+++ RosenW-WebServer/server.py
@@ -13,6 +13,7 @@ class Server:
 
     def __init__(self, server_address):
         self.listen_socket = listen_socket = socket.socket(self.address_family, self.socket_type)
+        self.listen_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         listen_socket.bind(server_address)
         listen_socket.listen(self.request_queue_size)
 
@@ -20,11 +21,17 @@ class Server:
         print 'Server Started'
         listen_socket = self.listen_socket
         while True:
-            # New client connection
-            self.client_connection, self.client_address = listen_socket.accept()
-            # Handle one request and close the client connection. Then
-            # loop over to wait for another client connection
-            self.handle_request()
+            try:
+                # New client connection
+                self.client_connection, self.client_address = listen_socket.accept()
+                # Handle one request and close the client connection. Then
+                # loop over to wait for another client connection
+                self.handle_request()
+            except Exception as e:
+                # self.client_connection.sendall("HTTP/1.1 500 INTERNAL SERVER ERROR\n\n")
+                print e
+            finally:
+                self.client_connection.close()
 
     def handle_request(self):
         #receive request data
@@ -48,29 +55,22 @@ class Server:
             '< {line}\n'.format(line=line)
             for line in request.splitlines()
         ))
-
         self.parse_request(request)
 
         # Construct a response and send it back to the client
         self.send_response(request)
-
     def parse_request(self, request):
         line = request.splitlines()[0]
         line = line.rstrip('\r\n')
         # Break down the request line into components
         (self.request_method,  # GET
-         self.whole_path,            # /hello
+         self.whole_path,      # /hello
          self.request_version  # HTTP/1.1
          ) = line.split()
 
     def send_response(self, request):
-        try:
-            response = self.dispatch_request(request)
-            self.client_connection.sendall(response)
-        except Exception as e:
-            print e
-        finally:
-            self.client_connection.close()
+        response = self.dispatch_request(request)
+        self.client_connection.sendall(response)
 
     def dispatch_request(self, request):
         params = []
@@ -102,7 +102,7 @@ class Server:
             }.get(self.path)
             return func()
         except Exception as e:
-            print 'No such route'
+            return "HTTP/1.1 404 NOT FOUND\n\n"
 
     def call_post_function(self):
         try:
@@ -111,7 +111,7 @@ class Server:
             }.get(self.path)
             return func()
         except Exception as e:
-            print 'PAGE DOES NOT EXIST'
+            return "HTTP/1.1 404 NOT FOUND\n\n"
 
     def get_sum(self):
         nums = []
@@ -121,8 +121,11 @@ class Server:
         return "HTTP/1.1 200 OK\n\n" + str(result)
 
     def get_file(self):
-        htmlf=codecs.open("./views/file.html", 'r', 'utf-8')
-        return "HTTP/1.1 200 OK\n\n" + htmlf.read()
+        try:
+            htmlf=codecs.open("./views/file.html", 'r', 'utf-8')
+            return "HTTP/1.1 200 OK\n\n" + htmlf.read()
+        except:
+            return "HTTP/1.1 404 NOT FOUND\n\n"
 
     def post_file(self):
         inside = False
@@ -151,8 +154,7 @@ class Server:
         print self.request
         return self.get_file()
 
-
-    def recv_timeout(self, the_socket, timeout=1):
+    def recv_timeout(self, the_socket, timeout=0.001):
         #make socket non blocking
         the_socket.setblocking(0)
 
@@ -179,7 +181,7 @@ class Server:
                     #change the beginning time for measurement
                     begin=time.time()
                 else:
-                    #sleep for sometime to indicate a gap
+                    # sleep for sometime to indicate a gap
                     time.sleep(0.1)
             except:
                 pass
@@ -188,5 +190,5 @@ class Server:
         return ''.join(total_data)
 
 if __name__ == '__main__':
-    server = Server(('', 8887))
+    server = Server(('', 8888))
     server.start()
