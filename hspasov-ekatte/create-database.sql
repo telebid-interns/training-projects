49a1e4d1c40212df5c91ad7aa793804203125bfe
Hristo Spasov <hristo@arc-global.org>
2018-06-15 17:01:08 +0300

    Fix database

--- hspasov-ekatte/create-database.sql
+++ hspasov-ekatte/create-database.sql
@@ -11,45 +11,27 @@
 -- $ psql --username=ekatte --password
 -- \i create-database.sql
 
-CREATE TABLE municipalities (
-  id TEXT NOT NULL PRIMARY KEY,
-  category INTEGER NOT NULL,
-  document TEXT NOT NULL
-);
-
 CREATE TABLE provinces (
   id TEXT NOT NULL PRIMARY KEY,
   region TEXT NOT NULL,
   document TEXT NOT NULL
 );
 
+CREATE TABLE municipalities (
+  id TEXT NOT NULL PRIMARY KEY,
+  category INTEGER NOT NULL,
+  document TEXT NOT NULL,
+  province_id TEXT NOT NULL,
+  FOREIGN KEY (province_id) REFERENCES provinces (id)
+);
+
 CREATE TABLE ekattes (
   id TEXT NOT NULL PRIMARY KEY,
   kind INTEGER NOT NULL,
   name TEXT NOT NULL,
-  province_id TEXT NOT NULL,
-  municipality_id TEXT NOT NULL,
-  municipal_gov_id TEXT NOT NULL,
   category INTEGER NOT NULL,
   altitude_code INTEGER NOT NULL,
-  tsb TEXT NOT NULL,
   document TEXT NOT NULL,
-  FOREIGN KEY (province_id) REFERENCES provinces (id),
-  FOREIGN KEY (municipality_id) REFERENCES municipalities (id)
-);
-
-CREATE TABLE municipality_ekattes (
-  ekatte_id TEXT NOT NULL,
   municipality_id TEXT NOT NULL,
-  FOREIGN KEY (ekatte_id) REFERENCES ekattes (id),
-  FOREIGN KEY (municipality_id) REFERENCES municipalities (id),
-  PRIMARY KEY (ekatte_id, municipality_id)
-);
-
-CREATE TABLE province_ekattes (
-  ekatte_id TEXT NOT NULL,
-  province_id TEXT NOT NULL,
-  FOREIGN KEY (ekatte_id) REFERENCES ekattes (id),
-  FOREIGN KEY (province_id) REFERENCES provinces (id),
-  PRIMARY KEY (ekatte_id, province_id)
+  FOREIGN KEY (municipality_id) REFERENCES municipalities (id)
 );
