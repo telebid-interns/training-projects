f8499f59cd568829e62106e064d644364080d128
Hristo Spasov <hristo@arc-global.org>
2018-06-15 11:29:23 +0300

    Add new create-databse sql

--- hspasov-ekatte/create-database.sql
+++ hspasov-ekatte/create-database.sql
@@ -11,118 +11,35 @@
 -- $ psql --username=ekatte --password
 -- \i create-database.sql
 
-CREATE TYPE document_kind AS ENUM (
-  'election_code',
-  'protocol',
-  'order',
-  'decree',
-  'ruling',
-  'official_document'
-);
-
-CREATE TABLE documents (
-  id TEXT NOT NULL PRIMARY KEY,
-  kind document_kind NOT NULL,
-  name TEXT NOT NULL,
-  institution TEXT NOT NULL,
-  doc_num TEXT NOT NULL,
-  doc_date DATE NOT NULL,
-  doc_act DATE NOT NULL,
-  state_gazette TEXT NOT NULL,
-  state_gazette_date DATE NOT NULL
-);
-
-CREATE TABLE regions (
-  id TEXT NOT NULL PRIMARY KEY,
-  name TEXT NOT NULL,
-  category TEXT,
-  document_id TEXT NOT NULL,
-  abc INTEGER NOT NULL,
-  FOREIGN KEY (document_id) REFERENCES documents (id)
-);
-
-CREATE TABLE regions_lvl_1 (
-  id TEXT NOT NULL PRIMARY KEY,
-  name TEXT NOT NULL,
-  document_id TEXT NOT NULL,
-  abc INTEGER NOT NULL,
-  FOREIGN KEY (document_id) REFERENCES documents (id)
-);
-
-CREATE TABLE regions_lvl_2 (
-  id TEXT NOT NULL PRIMARY KEY,
-  name TEXT NOT NULL,
-  document_id TEXT NOT NULL,
-  FOREIGN KEY (document_id) REFERENCES documents (id)
-);
-
-CREATE TABLE tsbs (
-  id TEXT NOT NULL PRIMARY KEY,
-  name TEXT NOT NULL
-);
-
 CREATE TABLE ekattes (
   id TEXT NOT NULL PRIMARY KEY,
   kind INTEGER NOT NULL,
   name TEXT NOT NULL,
-  province_id INTEGER, -- can be null if ekatte record is province
-  municipality_id INTEGER, -- can be null if ekatte record is municipality
-  municipal_gov_id INTEGER, -- can be null if ekatte record is municipality government
+  province_id TEXT, -- can be null if record is a province
+  municipality_id TEXT, -- can be null if record is a municipality
+  municipal_gov_id TEXT NOT NULL, -- if new table 'municipal_governemts' is created, NOT NULL constraint should be removed
   category INTEGER NOT NULL,
   altitude_code INTEGER NOT NULL,
-  document_id TEXT NOT NULL,
-  tsb_id TEXT NOT NULL,
-  FOREIGN KEY (document_id) REFERENCES documents (id),
-  FOREIGN KEY (tsb_id) REFERENCES tsbs (id)
-);
-
-CREATE TABLE sofia_settlements (
-  id TEXT NOT NULL PRIMARY KEY,
-  ekatte_id TEXT NOT NULL,
-  document_id TEXT NOT NULL,
-  FOREIGN KEY (ekatte_id) REFERENCES ekattes (id),
-  FOREIGN KEY (document_id) REFERENCES documents (id)
+  tsb TEXT NOT NULL, -- if new table 'tsbs' is created, NOT NULL should be removed
+  document TEXT NOT NULL -- if new table 'documents' is created, NOT NULL should be removed
 );
 
 CREATE TABLE municipalities (
-  id INTEGER NOT NULL PRIMARY KEY,
+  id TEXT NOT NULL PRIMARY KEY,
   ekatte_id TEXT NOT NULL,
-  document_id TEXT NOT NULL,
   category INTEGER NOT NULL,
-  FOREIGN KEY (ekatte_id) REFERENCES ekattes (id),
-  FOREIGN KEY (document_id) REFERENCES documents (id)
-);
-
-CREATE TABLE municipality_governments (
-  id INTEGER NOT NULL PRIMARY KEY,
-  ekatte_id TEXT NOT NULL,
-  document_id TEXT NOT NULL,
-  category TEXT, -- do not exist yet
-  FOREIGN KEY (ekatte_id) REFERENCES ekattes (id),
-  FOREIGN KEY (document_id) REFERENCES documents (id)
+  document TEXT NOT NULL,
+  FOREIGN KEY (ekatte_id) REFERENCES ekattes (id)
 );
 
 CREATE TABLE provinces (
-  id INTEGER NOT NULL PRIMARY KEY,
+  id TEXT NOT NULL PRIMARY KEY,
   ekatte_id TEXT NOT NULL,
-  document_id TEXT NOT NULL,
-  region_id TEXT NOT NULL,
-  FOREIGN KEY (ekatte_id) REFERENCES ekattes (id),
-  FOREIGN KEY (document_id) REFERENCES documents (id),
-  FOREIGN KEY (region_id) REFERENCES regions_lvl_2(id)
-);
-
-CREATE TABLE settlement_formations (
-  ekatte_id TEXT NOT NULL PRIMARY KEY,
-  kind INTEGER NOT NULL,
-  name TEXT NOT NULL,
-  area_1 TEXT NOT NULL,
-  area_2 TEXT, -- only some settlement formations have two areas
-  document_id TEXT NOT NULL,
-  FOREIGN KEY (document_id) REFERENCES documents(id)
+  region TEXT NOT NULL,
+  document TEXT NOT NULL,
+  FOREIGN KEY (ekatte_id) REFERENCES ekattes (id)
 );
 
 ALTER TABLE ekattes
-ADD FOREIGN KEY (province_id) REFERENCES provinces(id),
-ADD FOREIGN KEY (municipality_id) REFERENCES municipalities(id),
-ADD FOREIGN KEY (municipal_gov_id) REFERENCES municipality_governments(id);
+ADD FOREIGN KEY (province_id) REFERENCES provinces (id),
+ADD FOREIGN KEY (municipality_id) REFERENCES municipalities (id);
