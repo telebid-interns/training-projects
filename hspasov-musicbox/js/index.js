3f7eaf38c696f78ef46e45b4592ae0456ba4a409
Hristo Spasov <hristo@arc-global.org>
2018-06-19 19:21:32 +0300

    Refactoring and add support for multiple channel subscription

--- hspasov-musicbox/js/index.js
+++ hspasov-musicbox/js/index.js
@@ -1,52 +1,69 @@
 const apiKey = 'AIzaSyDe2NF-3q_aCIi1TIW0bIN44OqHQAPEc5w';
 const searchMaxResults = 1;
 
-const channelLinkInput = document.getElementById('channel-link-input');
 const channelLinkSubmit = document.getElementById('channel-link-submit');
 
 function channelLinkSubmitOnClick() {
+  const channelLinkInput = document.getElementById('channel-link-input');
+  const channelLinkInput2 = document.getElementById('channel-link-input2');
 
-  console.log(channelLinkInput.value);
+  const channelLinks = [channelLinkInput.value, channelLinkInput2.value];
+  
+  channelLinks.forEach(async link => {
+    const { type, value } = getChannelIdentificator(link);
 
-  if (channelLinkInput.value.match(/\/channel(?=\/.+)/)) {
+    let channelId;
+  
+    if (type === 'username') {
+      channelId = await getChannelId(value);
+    } else if (type === 'channelId') {
+      channelId = value;
+    } else {
+      // error
+      return;
+    }
+  
+    await getChannelVideos(channelId);
+  });
 
-    const channelIdMatches = channelLinkInput.value.match(/\/channel\/[^/\s]+/);
-    const result = channelIdMatches[0].replace('/channel/', '');
+  console.log('finished');
+}
+
+function getChannelIdentificator(channelAddress) {
+  // identificator can be channel id or username
+  const channelRoutePattern = /\/channel\/[^/\s]+/; // matches '/channel/channelId' in 'https://www.youtube.com/channel/channelId'
+  const usernameRoutePattern = /\/user\/[^/\s]+/; // matches '/user/username' in 'https://www.youtube.com/user/username'
+  const usernameShortRoutePattern = /.com\/[^/\s]+/; // matches '.com/username' in 'https://www.youtube.com/username'
+
+  if (channelAddress.match(channelRoutePattern)) {
+
+    const channelIdMatches = channelAddress.match(channelRoutePattern);
+    const result = channelIdMatches[0].replace('/channel/', '').trim();
     
-    getChannelVideos(result).then(() => {
-      console.log('finished');
-    }).catch(error => {
-      console.log(error);
-    });
-  } else if (channelLinkInput.value.match(/\/user(?=\/.+)/)) {
+    return {
+      type: 'channelId',
+      value: result
+    };
 
-    const usernameMatches = channelLinkInput.value.match(/\/user\/[^/\s]+/);
-    const result = usernameMatches[0].replace('/user/', '');
-    console.log(result);
+  } else if (channelAddress.match(usernameRoutePattern)) {
+
+    const usernameMatches = channelAddress.match(usernameRoutePattern);
+    const result = usernameMatches[0].replace('/user/', '').trim();
     
-    getChannelId(result).then(channelId => {
-      console.log(`Here is the channel id: ${channelId}`);
-      return getChannelVideos(channelId);
-    }).then(() => {
-      console.log('finished');
-    }).catch(error => {
-      console.log(error);
-    });
+    return {
+      type: 'username',
+      value: result
+    };
 
-  } else if (channelLinkInput.value.match(/.com\/[^/\s]+/)) {
+  } else if (channelAddress.match(usernameShortRoutePattern)) {
 
-    const usernameMatches = channelLinkInput.value.match(/.com\/[^/\s]+/);
-    const result = usernameMatches[0].replace('.com/', '');
-    console.log(result);
-    
-    getChannelId(result).then(channelId => {
-      console.log(`Here is the channel id: ${channelId}`);
-      return getChannelVideos(channelId);
-    }).then(() => {
-      console.log('finished');
-    }).catch(error => {
-      console.log(error);
-    });
+    const usernameMatches = channelAddress.match(usernameShortRoutePattern);
+    const result = usernameMatches[0].replace('.com/', '').trim();
+
+    return {
+      type: 'username',
+      value: result
+    };
 
   } else {
     // error - invalid input
@@ -72,11 +89,23 @@ async function getChannelVideos(channelId, pagesLeft=20, pageToken='') {
 }
 
 async function getVideoData(videoId) {
+  const artistPattern = /^.+(?= - )/; // matches 'Artist Name' in 'Artist Name - Song name (remix)'
+  const titlePattern = / - [^\(\)\[\]]+/; // matches ' - Song name ' in 'Artist Name - Song name (remix)'
+
   const response = await fetch(`https://www.googleapis.com/youtube/v3/videos?key=${apiKey}&part=contentDetails,snippet&id=${videoId}`);
   const videoData = await response.json();
-  console.log(videoData.items[0].snippet.title);
-  console.log('consoled log video data');
-  return;
+
+  const videoTitle = videoData.items[0].snippet.title;
+  
+  const artistMatches = videoTitle.match(artistPattern);
+  const artist = artistMatches[0].trim();
+
+  const songTitleMatches = videoTitle.match(titlePattern);
+  const songTitle = songTitleMatches[0].replace(' - ', '').trim();
+
+  console.log(artist);
+  console.log(songTitle);
+  return { artist, songTitle };
 }
 
 channelLinkSubmit.onclick = channelLinkSubmitOnClick;
