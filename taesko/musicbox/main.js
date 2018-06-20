c33a703794f03ac2081e5e335498e8eaa4b4a4d5
Antonio Todorov <taeskow@gmail.com>
2018-06-19 23:22:25 -0700

    Get channel videos.

--- taesko/musicbox/main.js
+++ taesko/musicbox/main.js
@@ -1,8 +1,30 @@
+var RESPONSE;
+
+function getChannelVideos(channel_id) {
+    let videos = [];
+    gapi.client.youtube.channels.list({
+        'id': channel_id,
+        'part': 'snippet,contentDetails'
+    }).then(function (response) {
+        let playlistId = response.result.items[0].contentDetails.relatedPlaylists.uploads
+        gapi.client.youtube.playlistItems.list({
+            'maxResults': 25,
+            'part': 'snippet,contentDetails',
+            'playlistId': playlistId
+        }).then(function (response) {
+            RESPONSE = response;
+            videos = response.result.items;
+            // response.result.items[0].snippet.title
+        })
+    });
+    return videos
+}
+
 function start() {
     // Initializes the client with the API key and the Translate API.
     gapi.client.init({
-      'apiKey': '***',
-      'discoveryDocs': ['https://www.googleapis.com/discovery/v1/apis/translate/v2/rest'],
+        'apiKey': '***',
+        'discoveryDocs': ['https://www.googleapis.com/discovery/v1/apis/translate/v2/rest'],
     });
     //     .then(function() {
     //   // Executes an API request, and returns a Promise.
@@ -18,14 +40,8 @@ function start() {
     //   console.log('Error: ' + reason.result.error.message);
     // });
     gapi.client.load('youtube', 'v3', function () {
-        console.log("youtube loaded");
-        let request = gapi.client.youtube.search.list({
-            q: "youtube is annoying",
-            part: "snippet"
-        });
-        request.execute(function (response) {
-            console.log(JSON.stringify(response.result))
-        })
+        // console.log("youtube loaded");
+        videos = getChannelVideos("UC_x5XG1OV2P6uZZ5FSM9Ttw")
     });
 }
 
