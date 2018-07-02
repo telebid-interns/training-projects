6b27c426ee2c8d98af84387df5c3942c42e54b80
Antonio Todorov <taeskow@gmail.com>
2018-07-02 18:22:55 +0300

    Add API skeleton to FreeFall.

--- taesko/freefall/js/main.js
+++ taesko/freefall/js/main.js
@@ -12,34 +12,24 @@ const WEEK_DAYS = [
     'Sunday',
 ];
 
-let ticketItemTemplate; // initialize DOM elements after document loads
-let uiRoutesList;
 
-class ApplicationError extends Error {
-    constructor (message) {
-        super(message);
-        alert('An unknown application error occurred. Please reload the page.<br>' +
-            message);
+class BaseError extends Error {
+    constructor (userMessage, ...logs) {
+        super(userMessage);
+        console.warn(...logs);
     }
 }
 
-class PeerError extends Error {
-    constructor (message) {
-        super(message);
+class ApplicationError extends BaseError {
+    constructor (userMessage, ...logs) {
+        super(userMessage, ...logs);
+        alert(userMessage);
     }
 }
 
-class UserError extends PeerError {
-    constructor (message) {
-        super(message);
-        alert('Good job.<br>' + message);
-    }
-}
-
-class ExternalError extends PeerError {
-    constructor (message, who) {
-        super(message);
-        alert(who + 'isn\'t working.<br>' + message);
+class PeerError extends BaseError {
+    constructor (userMessage, ...logs) {
+        super(userMessage, ...logs);
     }
 }
 
@@ -64,92 +54,252 @@ AIRPORT_MAP = {
     },
 };
 
-function fetchRoutes (
+function getAirportByString (string) {
+    return string;
+}
+
+const EXAMPLE_ROUTES = {
+    status_code: 1000,
+    currency: 'USD',
+    routes: [
+        {
+            booking_token: 'solid booking token',
+            price: 170,
+            route: [
+                {
+                    airpot_from: 'SOF',
+                    airport_to: 'BBU',
+                    city_from: 'Sofia',
+                    city_to: 'Bucarest',
+                    dtime: '2018-06-28T16:30',
+                    atime: '2018-06-28T17:00',
+                    airline_logo: 'https://www.designevo.com/res/templates/thumb_small/glorious-sunrise-aviation.png',
+                    airline_name: 'International bureo of investigation of plane crashes',
+                    flight_number: '414HU4KB4R',
+                },
+                {
+                    airport_from: 'BBU',
+                    airport_to: 'HND',
+                    city_from: 'Bucarest',
+                    city_to: 'Tokyo',
+                    dtime: '2018-07-01T06:30',
+                    atime: '2018-07-01T12:30',
+                    airline_logo: 'https://www.designevo.com/res/templates/thumb_small/glorious-sunrise-aviation.png',
+                    airline_name: 'International bureo of investigation of plane crashes',
+                    flight_number: 'NAN1',
+                },
+            ],
+        },
+    ],
+};
+
+function snakeToCamel(string) {
+    let words = string.split('_');
+    let top = words[0];
+
+    words.splice(0, 1);
+
+    return top + words.map(_.capitalize).join('');
+}
+
+
+function switchStyle(json, converter) {
+    function switchHash(hash) {
+        return _.mapKeys(hash, (value, key) => converter(key))
+    }
+
+    function switcher(json) {
+        let converted = switchHash(json);
+
+        for (let [key, value] of Object.entries(converted)) {
+            if($.isPlainObject(value)) {
+                value = switcher(value);
+            } else if (Array.isArray(value)) {
+                value = value.map(switcher);
+            }
+
+            converted[key] = value;
+        }
+
+        return converted;
+    }
+
+    return switcher(json);
+}
+
+let jsonRPCRequestId = 1;
+
+async function jsonRPCRequest (method, params) {
+    let url = '10.20.1.155:3000/?format=json';
+    let request = {
+        jsonrpc: 2.0,
+        method,
+        params,
+        id: jsonRPCRequestId,
+    };
+    let response;
+
+    try {
+        response = await $.post(url, request, null, 'json');
+    } catch (error) {
+        throw new PeerError('Service is not available at the moment',
+            'failed to make a post request to server API',
+            'url: ', url,
+            'request data: ', request,
+            'error raised: ', error);
+    } finally {
+        jsonRPCRequestId++;
+    }
+
+    if (!['jsonrpc', 'id'].every(prop => _.has(response, prop))) {
+        throw new PeerError('Service is not available at the moment',
+            'Invalid JSON RPC response from server');
+    } else if (_.has(response, 'error')) {
+        throw new PeerError('Service is not available at the moment',
+            'jsonrpc request failed.',
+            'sent data: ', request,
+            'got response: ', response);
+    } else if (!_.has(response, 'result')) {
+        throw new PeerError('Service is not available at the moment',
+            'jsonrpc request failed - response has neither \'error\' nor \'result\' properties',
+            'sent data: ', request,
+            'got response: ', response);
+    } else if (response.id !== request.id) {
+        throw new ApplicationError(
+            'An unexpected behaviour occurred. Please refresh the page.',
+            'json rpc response and request id are out of sync',
+            'request id =', request.id,
+            'response id =', response.id);
+    }
+
+    return response;
+}
+
+async function fetchRoutes (
     {
         flyFrom,
         flyTo,
         priceTo,
         currency = 'USD',
-        dateFrom = new Date(),
+        dateFrom,
         dateTo,
-        sort = 'price',
-        maxFlyDuration = 6,
-    }){
-    return {
-        currency: 'USD',
-        routes: [
-            {
-                bookingToken: "this here is a very very valid booking token",
-                route: [
-                    {
-                        airportFrom: 'SOF',
-                        airportTo: 'BBU',
-                        cityFrom: 'Sofia',
-                        cityTo: 'Bucarest',
-                        price: 20,
-                        dtime: new Date("2018-06-28T16:30"),
-                        atime: new Date("2018-06-28T17:00"),
-                        airlineLogo: "https://www.designevo.com/res/templates/thumb_small/glorious-sunrise-aviation.png",
-                        airlineName: "International bureo of investigation of plane crashes",
-                        flightNumber: "414HU4KB4R"
-                    },
-                    {
-                        airportFrom: 'BBU',
-                        airportTo: 'HND',
-                        cityFrom: 'Bucarest',
-                        cityTo: 'Tokyo',
-                        price: 150,
-                        dtime: new Date('2018-07-01T06:30'),
-                        atime: new Date('2018-07-01T12:30'),
-                        airlineLogo: "https://www.designevo.com/res/templates/thumb_small/glorious-sunrise-aviation.png",
-                        airlineName: "International bureo of investigation of plane crashes",
-                        flightNumber: 'NAN1'
-                    }
-                ]
-            }
-        ]
+        sort,
+        maxFlyDuration,
+    }) {
+    let params = {
+        fly_from: flyFrom,
+        fly_to: flyTo,
+        price_to: priceTo,
+        currency: currency,
+        date_from: dateFrom,
+        date_to: dateTo,
+        sort: sort,
+        max_fly_duration: maxFlyDuration,
+    };
+    const requiredParams = ['fly_from', 'fly_to'];
+    const fixedParams = {sort: ['price', 'duration'], currency: ['USD', 'BGN']};
+
+    for (let requiredParam of requiredParams) {
+        if (Object.keys(params).indexOf(requiredParam) === -1 ||
+            params[requiredParam] === undefined)
+            throw new ApplicationError(
+                'An unexpected behaviour occurred. Please refresh the page.',
+                'Missing required keyword argument: ', requiredParam,
+                'to call of', fetchRoutes);
     }
+
+    for (let [fixedParam, possibleStates] of Object.entries(fixedParams)) {
+        if (
+            Object.keys(params).indexOf(fixedParam) !== -1 &&
+            possibleStates.indexOf(params[fixedParam]) === -1
+        ) {
+            throw new ApplicationError(
+                'An unexpected behaviour occurred. Please refresh the page.',
+                'Paramater', fixedParam,
+                'is not one of:', fixedParams[fixedParam],
+                'instead got -', params[fixedParam]);
+        }
+    }
+
+    // TODO is this needed ?
+    for (let [key, value] of Object.entries(params)) {
+        if (value === undefined) {
+            delete params[key];
+        }
+    }
+
+    console.log("request paramaters are: ", params);
+    // let response = await jsonRPCRequest('search', params);
+    let response = switchStyle(EXAMPLE_ROUTES, snakeToCamel);
+
+    for (let routeObj of response.routes) {
+        routeObj.price += response.currency;
+
+        for (let flight of routeObj.route) {
+            flight.dtime = new Date(flight.dtime);
+            flight.atime = new Date(flight.atime);
+        }
+    }
+    return response;
 }
 
 
-function displayRoutes(routes) {
-    function timeStringFromDate(date) {
-        return `${date.getUTCHours()}:${date.getUTCMinutes()}`;
+function displayRoutes (routes, routesList, routeItemTemplate, flightItemTemplate) {
+    routesList.find('li:not(:first)').remove();
+
+    for(let route of routes.routes) {
+        let clone = routeItemTemplate.clone()
+            .removeAttr('id')
+            .removeClass('hidden');
+        let routeList = clone.find('ul');
+        let newRoute = makeFlightList(route.route, routeList, flightItemTemplate);
+
+        clone.find('.route-price').text(route.price);
+        routesList.append(clone.append(newRoute));
     }
 
-    function weeklyDateString(date) {
-        let monthName = MONTH_NAMES[date.getMonth()];
-        let dayName = WEEK_DAYS[date.getDay()];
+    function makeFlightList (route, list, flightItemTemplate) {
+        list.find('li:not(:first)').remove();
 
-        return `${dayName} ${date.getDate()} ${monthName}`;
-    }
+        for (let flight of route) {
+            list.append(makeListItem(flight, flightItemTemplate));
+        }
 
-    function makeListItem(flight) {
-        let clone = ticketItemTemplate.clone().
-            removeAttr('id').
-            removeClass('hidden');
-        clone.find('.price').text(flight.price);
-        clone.find('.airline-logo').text(); // TODO fill
-        clone.find('.airline-name').text('example airline name');
-        clone.find('.departure-time').text(timeStringFromDate(flight.dtime));
-        clone.find('.arrival-time').text(timeStringFromDate(flight.atime));
-        clone.find('.flight-date').text(weeklyDateString(flight.dtime));
-        clone.find('.timezone').text('UTC');
-        clone.find('.duration').text(flight.duration / 1000 / 60 / 60 + 'hours');
-        clone.find('.from-to-display').
-            text(
-                `${flight.cityFrom} -----> ${flight.cityTo}`);
-        return clone;
-    }
+        list.show();
 
-    uiRoutesList.find('li:not(:first)').remove();
-    console.log(routes);
-    for (let flight of routes.routes[0]) {
-        uiRoutesList.append(makeListItem(flight));
+        return list;
+
+        function makeListItem (flight, itemTemplate) {
+            let clone = itemTemplate.clone().
+                removeAttr('id').
+                removeClass('hidden');
+
+            let duration = flight.atime.getTime() - flight.dtime.getTime();
+
+            clone.find('.airline-logo').text(); // TODO fill
+            clone.find('.airline-name').text('example airline name');
+            clone.find('.departure-time').text(timeStringFromDate(flight.dtime));
+            clone.find('.arrival-time').text(timeStringFromDate(flight.atime));
+            clone.find('.flight-date').text(weeklyDateString(flight.dtime));
+            clone.find('.timezone').text('UTC');
+            clone.find('.duration').text(duration / 1000 / 60 / 60 + ' hours');
+            clone.find('.from-to-display').text(`${flight.cityFrom} -----> ${flight.cityTo}`);
+
+            return clone;
+        }
+
+        function timeStringFromDate (date) {
+            return `${date.getUTCHours()}:${date.getUTCMinutes()}`;
+        }
+
+        function weeklyDateString (date) {
+            let monthName = MONTH_NAMES[date.getMonth()];
+            let dayName = WEEK_DAYS[date.getDay()];
+
+            return `${dayName} ${date.getDate()} ${monthName}`;
+        }
     }
-    uiRoutesList.show();
 }
-
 
 function watchInputField (inputField, callback) {
     let lastValue = '';
@@ -194,14 +344,15 @@ function setupAutoComplete ({hash, textInput, dataList}) {
 }
 
 $(document).ready(() => {
-        ticketItemTemplate = $('#routes-container ul li').first();
-        uiRoutesList = $('#routes-container ul');
+        let allRoutesList = $('#all-routes-list');
+        let flightsListTemplate = $('#flights-list-item-template');
+        let flightItemTemplate = $('#flight-item-template');
 
         let flightForm = $('#flight-form-input');
         let flightFormData = '';
 
         flightForm.on('submit',
-            event => {
+            async event => {
                 event.preventDefault();
 
                 if (flightForm.serialize() === flightFormData) {
@@ -209,7 +360,11 @@ $(document).ready(() => {
                 }
 
                 flightFormData = flightForm.serialize();
-                displayRoutes(fetchRoutes({}));
+
+                let flyFrom = getAirportByString($('#departure-input').val());
+                let flyTo = getAirportByString($('#arrival-input').val());
+
+                displayRoutes(await fetchRoutes({flyFrom, flyTo}), allRoutesList, flightsListTemplate, flightItemTemplate);
 
                 return false;
             });
