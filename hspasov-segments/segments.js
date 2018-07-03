2d716d94c4d5b083433670dfa33548f8d7ab6962
Hristo Spasov <hristo@arc-global.org>
2018-07-03 12:05:18 +0300

    Add validation

--- hspasov-segments/segments.js
+++ hspasov-segments/segments.js
@@ -1,7 +1,29 @@
+function assert(assertion, errMsg) {
+  if (!assertion) {
+    console.error(errMsg);
+  }
+}
+
 const n = 20;
 const a = 2;
 const b = 5;
 const c = 2;
+
+assert(
+  Number.isInteger(n) &&
+  Number.isInteger(a) &&
+  Number.isInteger(b) &&
+  Number.isInteger(c) &&
+  n > 0 &&
+  a > 0 &&
+  b > 0 &&
+  c > 0 &&
+  n < 100000 &&
+  a < 100000 &&
+  b < 100000 &&
+  c < 100000,
+  'Invalid input.'
+);
 
 const points = new Array(n + 1);
 const redPaths = new Array(n);
@@ -20,6 +42,7 @@ for (let i = 0; i < points.length; i++) {
 }
 
 let beginning = 0;
+
 for (let i = 1; i < points.length; i++) {
   if (points[i]) {
     if (i - beginning === c) {
@@ -35,7 +58,9 @@ for (let i = 1; i < points.length; i++) {
 let result = 0;
 
 for (let i = 0; i < redPaths.length; i++) {
-  if (!redPaths[i]) result++;
+  if (!redPaths[i])  {
+    result++;
+  }
 }
 
 console.log(result);
