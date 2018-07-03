a153c7a05bda16281eaac49191d45548d0df57b1
Hristo Spasov <hristo@arc-global.org>
2018-07-03 14:44:29 +0300

    Add simple visualization

--- hspasov-segments/segments.js
+++ hspasov-segments/segments.js
@@ -1,61 +1,84 @@
-function assert(assertion, errMsg) {
-  if (!assertion) {
-    console.error(errMsg);
-  }
+const visualizationElement = document.getElementById('visualization');
+const nInputElement = document.getElementById('n-input');
+const aInputElement = document.getElementById('a-input');
+const bInputElement = document.getElementById('b-input');
+const cInputElement = document.getElementById('c-input');
+const submitElement = document.getElementById('submit');
+const resultElement = document.getElementById('result');
+
+const redSegment = '<span class="redsegment"><b>=</b></span>';
+const segment = '<span class="segment">_</span>';
+
+function handleSubmitElement () {
+  solve(Number(nInputElement.value), Number(aInputElement.value), Number(bInputElement.value), Number(cInputElement.value));
 }
 
-const n = 20;
-const a = 3;
-const b = 2;
-const c = 1;
+function solve (n, a, b, c) {
+  let visualization = '';
 
-assert(
-  Number.isInteger(n) &&
-  Number.isInteger(a) &&
-  Number.isInteger(b) &&
-  Number.isInteger(c) &&
-  n > 0 &&
-  a > 0 &&
-  b > 0 &&
-  c > 0 &&
-  n < 100000 &&
-  a < 100000 &&
-  b < 100000 &&
-  c < 100000,
-  'Invalid input.'
-);
+  if (!(
+    Number.isInteger(n) &&
+    Number.isInteger(a) &&
+    Number.isInteger(b) &&
+    Number.isInteger(c) &&
+    n > 0 &&
+    a > 0 &&
+    b > 0 &&
+    c > 0 &&
+    n < 100000 &&
+    a < 100000 &&
+    b < 100000 &&
+    c < 100000
+  )) {
 
-const points = new Array(n + 1);
-const redPaths = new Array(n);
-
-points.fill(false);
-redPaths.fill(false);
-
-for (let i = 0; i < points.length; i++) {
-  if (i % a === 0) {
-    points[i] = true;
+    console.error('Invalid input');
+    visualizationElement.innerHTML = 'Error! Please use positive integers (1, 2, 3 ...), but smaller than 100 000!';
+    return;
   }
 
-  if (i % b == 0) {
-    points[points.length - i - 1] = true;
+  const points = new Array(n + 1);
+  const redSegments = new Array(n);
+
+  points.fill(false);
+  redSegments.fill(false);
+
+  for (let i = 0; i < points.length; i++) {
+    if (i % a === 0) {
+      points[i] = true;
+    }
+
+    if (i % b == 0) {
+      points[points.length - i - 1] = true;
+    }
   }
+
+  for (let i = 0; i < points.length - c; i++) {
+    if (points[i] && points[i + c]) {
+      redSegments.fill(true, i, i + c);
+    }
+  }
+
+  // console.log(points);
+  // console.log(redSegments);
+
+  let result = 0;
+
+  for (let i = 0; i < redSegments.length; i++) {
+    visualization += (points[i]) ? '.' : ' ';
+
+    if (!redSegments[i])  {
+      result++;
+      visualization += segment;
+    } else {
+      visualization += redSegment;
+    }
+  }
+
+  resultElement.innerHTML = `Result: ${result}`;
+
+  visualization += '.';
+
+  visualizationElement.innerHTML = visualization;
 }
 
-for (let i = 0; i < points.length - c; i++) {
-  if (points[i] && points[i + c]) {
-    redPaths.fill(true, i, i + c);
-  }
-}
-
-// console.log(points);
-// console.log(redPaths);
-
-let result = 0;
-
-for (let i = 0; i < redPaths.length; i++) {
-  if (!redPaths[i])  {
-    result++;
-  }
-}
-
-console.log(result);
+submitElement.onclick = handleSubmitElement;
