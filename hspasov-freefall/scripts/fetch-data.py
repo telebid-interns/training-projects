70bffb852794c685dea8134c8c3dc5e69d2c95ed
Hristo Spasov <hristo@arc-global.org>
2018-07-12 19:45:29 +0300

    Add asserts and error classes

--- hspasov-freefall/scripts/fetch-data.py
+++ hspasov-freefall/scripts/fetch-data.py
@@ -2,6 +2,38 @@ import urllib.request
 import json
 import sqlite3
 
+class BaseError(Exception):
+	def __init__(self, msg):
+		super().__init__(msg)
+		print(msg)
+
+class AppError(BaseError):
+	def __init__(self, msg):
+		super().__init__(msg)
+		print(self.msg)
+		print('inside app error')
+
+class PeerError(BaseError):
+	def __init__(self, msg):
+		self.msg = msg
+
+class UserError(BaseError):
+	def __init__(self, msg):
+		self.msg = msg
+
+def assertApp(condition, msg):
+	if not condition:
+		raise AppError(msg)
+
+def assertPeer(condition, msg):
+	if not condition:
+		raise PeerError(msg)
+
+def assertUser(condition, msg):
+	if not condition:
+		raise UserError(msg)
+
+
 conn = sqlite3.connect('../freefall.db')
 c = conn.cursor()
 c.execute('SELECT * FROM airports;')
@@ -10,3 +42,5 @@ print(c.fetchall())
 contents = urllib.request.urlopen("https://api.skypicker.com/airlines").read()
 parsed_airlines = json.loads(contents.decode('utf-8'))
 print(parsed_airlines[0]['id'])
+
+assertApp(False, 'it works')
