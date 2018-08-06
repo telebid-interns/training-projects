1776653f77a0a2dc958cca5d1dc0579fb50dee03
Antonio Todorov <taeskow@gmail.com>
2018-08-06 15:23:59 +0300

    wip css admin.

---
+++ taesko/css_admin/main.js
@@ -0,0 +1,26 @@
+const gallery = document.getElementById('gallery');
+let sidebarShowed = true;
+
+document.getElementById('add-button').addEventListener('click', () => {
+  const li = document.createElement('li');
+  const img = document.createElement('img');
+  img.setAttribute('src', 'hell.jpg');
+  li.appendChild(img);
+  gallery.appendChild(li);
+});
+
+document.getElementById('remove-button').addEventListener('click', () => {
+  gallery.removeChild(gallery.children[0]);
+});
+
+document.getElementById('menu-button').addEventListener('click', () => {
+  if (sidebarShowed) {
+    document.getElementById('sidebar').classList.add('hide-sidebar');
+    document.getElementById('main-content').classList.add('fill-sidebar');
+    sidebarShowed = false;
+  } else {
+    document.getElementById('sidebar').classList.remove('hide-sidebar');
+    document.getElementById('main-content').classList.remove('fill-sidebar');
+    sidebarShowed = true;
+  }
+});
