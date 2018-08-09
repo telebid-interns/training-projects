55594dd0881d9fb363d8d3275c33be5291db64c1
Rosen <ros.miryanov@gmail.com>
2018-08-09 17:54:03 +0300

    ,

--- RosenW-DaliPeche/node-koa/src/server/server.js
+++ RosenW-DaliPeche/node-koa/src/server/server.js
@@ -24,8 +24,70 @@ const {
   ADMIN_USERNAME,
   ADMIN_PASSWORD,
   MAX_REQUESTS_PER_HOUR,
-  MAXIMUM_CREDITS_ALLOWED
+  MAXIMUM_CREDITS_ALLOWED,
+  MERCHANT_ID,
+  CREDIT_CARD_PRIVATE_KEY,
+  CREDIT_CARD_PUBLIC_KEY
 } = require('./../utils/consts.js');
+const braintree = require('braintree');
+
+const gateway = braintree.connect({
+    environment: braintree.Environment.Sandbox,
+    merchantId: MERCHANT_ID,
+    publicKey: CREDIT_CARD_PUBLIC_KEY,
+    privateKey: CREDIT_CARD_PRIVATE_KEY
+});
+
+gateway.config.timeout = 10000;
+
+//set up merchant account
+
+merchantAccountParams = {
+    individual: {
+        firstName: "Jane",
+        lastName: "Doe",
+        email: "jane@14ladders.com",
+        phone: "5553334444",
+        dateOfBirth: "1981-11-19",
+        ssn: "456-45-4567",
+        address: {
+            streetAddress: "111 Main St",
+            locality: "Chicago",
+            region: "IL",
+            postalCode: "60622"
+        }
+    },
+    business: {
+        legalName: "Jane's Ladders",
+        dbaName: "Jane's Ladders",
+        taxId: "98-7654321",
+        address: {
+            streetAddress: "111 Main St",
+            locality: "Chicago",
+            region: "IL",
+            postalCode: "60622"
+        }
+    },
+    funding: {
+        descriptor: "Blue Ladders",
+        destination: braintree.MerchantAccount.FundingDestination.Bank,
+        email: "mailsender6000@gmail.com",
+        mobilePhone: "5555555555",
+        accountNumber: "1123581321",
+        routingNumber: "071101307"
+    },
+    tosAccepted: true,
+    masterMerchantAccountId: "dalipeche123",
+    id: "dalipeche"
+};
+
+gateway.merchantAccount.create(merchantAccountParams, function(err, result) {
+  if (err) {
+    console.log(`MerchantAccount Error: ${err}`);
+    return;
+  }
+  console.log(`MerchantAccount No Error, result: ${JSON.stringify(result)}`);
+});
 
 const app = new Koa();
 
@@ -306,10 +368,12 @@ router.get('/admin/ctransfers', async (ctx, next) => {
 router.get('/buy', async (ctx, next) => {
   trace(`GET '/buy'`);
 
-  await ctx.render('buy',
-    {
+  const response = await gateway.clientToken.generate();
+
+  await ctx.render('buy', {
       success: ctx.query.success,
-      error: ctx.query.err
+      error: ctx.query.err,
+      clientToken: response.clientToken
     }
   );
 });
@@ -317,30 +381,46 @@ router.get('/buy', async (ctx, next) => {
 // POST buy
 router.post('/buy', async (ctx, next) => {
   trace(`POST '/buy'`);
-  if (ctx.session.user == null) {
-    ctx.redirect('/home');
+  // if (ctx.session.user == null) {
+  //   ctx.redirect('/home');
+  // }
+
+  const sale = await gateway.transaction.sale({
+    amount: "10.00",
+    paymentMethodNonce: ctx.request.body.nonce,
+    options: {
+        submitForSettlement: true
+    }
+  });
+  console.log(sale);
+
+  if (sale.success) {
+    console.log('ss');
+  } else {
+    console.log('not ss');
   }
+  return;
 
-  assert(isObject(ctx.request.body), 'Post buy has no body', 12);
+  // assert(isObject(ctx.request.body), 'Post buy has no body', 12);
 
-  const credits = ctx.request.body.credits;
+  // const credits = ctx.request.body.credits;
 
-  const user = (await db.query(`SELECT * FROM users WHERE username = $1`, ctx.session.user)).rows[0];
-  assert(isObject(user), 'User not an object', 13);
+  // const user = (await db.query(`SELECT * FROM users WHERE username = $1`, ctx.session.user)).rows[0];
+  // assert(isObject(user), 'User not an object', 13);
 
-  if (!isInteger(Number(credits)) || Number(credits) <= 0) {
-    await ctx.render('buy', { error: 'Credits must be a positive whole number' });
-    return next();
-  }
+  // if (!isInteger(Number(credits)) || Number(credits) <= 0) {
+  //   await ctx.render('buy', { error: 'Credits must be a positive whole number' });
+  //   return next();
+  // }
 
-  if (Number(credits) + Number(user.credits) > MAXIMUM_CREDITS_ALLOWED) {
-    await ctx.render('buy', { error: 'Maximum credits allowed is 1000000 per user' });
-    return next();
-  }
+  // if (Number(credits) + Number(user.credits) > MAXIMUM_CREDITS_ALLOWED) {
+  //   await ctx.render('buy', { error: 'Maximum credits allowed is 1000000 per user' });
+  //   return next();
+  // }
 
-  await addCreditsToUser(user, credits);
+  // await addCreditsToUser(user, credits);
 
-  await ctx.render('buy', { credits });
+  // await ctx.render('buy', { credits });
 });
 
 const addCreditsToUser = async (user, credits) => {
