e6f43614ebdfe575a2849279a504b736dd320cf7
Rosen <ros.miryanov@gmail.com>
2018-08-10 16:37:29 +0300

    indexed db

--- RosenW-DaliPeche/node-koa/src/database/dbschema.sql
+++ RosenW-DaliPeche/node-koa/src/database/dbschema.sql
@@ -58,3 +58,36 @@ CREATE TABLE credit_transfers (
   event text NOT NULL,
   transfer_date timestamp NOT NULL
 );
+
+CREATE INDEX requests_iata_code_index ON requests (iata_code);
+CREATE INDEX requests_city_index ON requests (city);
+CREATE INDEX requests_call_count_index ON requests (call_count);
+
+CREATE INDEX cities_name_index ON cities (name);
+CREATE INDEX cities_country_code_index ON cities (country_code);
+CREATE INDEX cities_lng_index ON cities (lng);
+CREATE INDEX cities_lat_index ON cities (lat);
+CREATE INDEX cities_observed_at_index ON cities (observed_at);
+
+CREATE INDEX weather_conditions_city_id_index ON weather_conditions (city_id);
+CREATE INDEX weather_conditions_weather_index ON weather_conditions (weather);
+CREATE INDEX weather_conditions_forecast_time_index ON weather_conditions (forecast_time);
+
+CREATE INDEX users_username_index ON users (username);
+CREATE INDEX users_email_index ON users (email);
+CREATE INDEX users_credits_index ON users (credits);
+CREATE INDEX users_failed_requests_index ON users (failed_requests);
+CREATE INDEX users_successful_requests_index ON users (successful_requests);
+CREATE INDEX users_password_index ON users (password);
+CREATE INDEX users_salt_index ON users (salt);
+CREATE INDEX users_date_registered_index ON users (date_registered);
+
+CREATE INDEX api_keys_key_index ON api_keys (key);
+CREATE INDEX api_keys_user_id_index ON api_keys (user_id);
+CREATE INDEX api_keys_use_count_index ON api_keys (use_count);
+
+CREATE INDEX credit_transfers_user_id_index ON credit_transfers (user_id);
+CREATE INDEX credit_transfers_credits_bought_index ON credit_transfers (credits_bought);
+CREATE INDEX credit_transfers_credits_spent_index ON credit_transfers (credits_spent);
+CREATE INDEX credit_transfers_event_index ON credit_transfers (event);
+CREATE INDEX credit_transfers_transfer_date_index ON credit_transfers (transfer_date);
