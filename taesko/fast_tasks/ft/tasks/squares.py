7ed7ae90b1f703e8ca84e7fbcbe90d32a04404b5
Antonio Todorov <taeskow@gmail.com>
2018-08-21 17:38:11 +0300

    Working solution

--- taesko/fast_tasks/ft/tasks/squares.py
+++ taesko/fast_tasks/ft/tasks/squares.py
@@ -1,3 +1,4 @@
+import itertools
 import math
 import collections
 import sys
@@ -6,8 +7,37 @@ import pprint
 Location = collections.namedtuple('Location', ['row', 'col'])
 
 
-def construct_square_map(length, symbols):
-    dct = {}
+class SquareMap(collections.UserDict):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._side_len = None
+        self._mini_square_len = None
+
+    @property
+    def side_len(self):
+        assert self._side_len
+        return self._side_len
+
+    @side_len.setter
+    def side_len(self, value):
+        assert isinstance(value, int)
+        self._side_len = value
+
+    @property
+    def mini_square_len(self):
+        return self._mini_square_len
+
+    @mini_square_len.setter
+    def mini_square_len(self, value):
+        assert isinstance(value, int)
+        self._mini_square_len = value
+
+
+def construct_square_map(mini_square_len, symbols):
+    dct = SquareMap()
+    length = mini_square_len * mini_square_len
+    dct.side_len = length
+    dct.mini_square_len = mini_square_len
 
     for row in range(length):
         for col in range(length):
@@ -30,32 +60,35 @@ def remove_symbol_from_locations(square_map, locations, symbol):
         allowed = square_map[loc]
 
         assert len(allowed) > 0, 'got {allowed}'.format(**locals())
-        assert len(allowed) > 2 or symbol not in allowed, '{symbol} is alone in {allowed}'.format(**locals())
+        assert len(allowed) > 1 or symbol not in allowed, '{symbol} is alone in {allowed}'.format(**locals())
 
         try:
             allowed.remove(symbol)
             modified = True
-        except ValueError:
+        except KeyError:
             pass
 
     return modified
 
 
 def locations_affected_by_location(square_map, location):
-    affected_rows = [Location(row, location.col) for row in range(len(square_map))]
-    affected_cols = [Location(location.row, col) for col in range(len(square_map))]
+    affected_rows = [Location(row, location.col) for row in range(square_map.side_len)]
+    affected_cols = [Location(location.row, col) for col in range(square_map.side_len)]
     affected_square = []
-    nested_square_len = int(math.sqrt(len(square_map)))
-    square_row = location.row // nested_square_len
-    square_col = location.col // nested_square_len
-    for row in range(square_row, nested_square_len):
-        for col in range(square_col, nested_square_len):
+    mini_square_len = square_map.mini_square_len
+    square_row = mini_square_len * (location.row // mini_square_len)
+    square_col = mini_square_len * (location.col // mini_square_len)
+    for row in range(square_row, square_row+mini_square_len):
+        for col in range(square_col, square_col+mini_square_len):
             affected_square.append(Location(row, col))
 
-    return set(affected_rows + affected_cols + affected_square)
+    affected = set(affected_rows + affected_cols + affected_square)
+    affected.remove(location)
+    return affected
 
 
 def solve(square_map):
+    print('Solving...')
     while True:
         modified_any = False
         finished = True
@@ -67,9 +100,10 @@ def solve(square_map):
                 modified_any = modified_any or modified
             else:
                 finished = False
-        assert not modified_any and finished, 'Unsolvable puzzle by algorithm'
         if finished:
             return square_map
+        else:
+            assert modified_any, 'Unsolvable puzzle by algorithm'
 
 
 def parse_input_file(file):
@@ -89,6 +123,7 @@ def parse_input_file(file):
     symbol_loc_map = {}
 
     for row, line in enumerate(lines):
+        print(line)
         symbols = [s for s in line.split() if s]
         for col, sym in enumerate(symbols):
             if sym == '0':
@@ -96,14 +131,23 @@ def parse_input_file(file):
             symbol_loc_map[Location(row, col)] = sym
 
     all_symbols = set(symbol_loc_map.values())
-    total_square_len = int(math.pow(mini_square_length, 4))
-    square_map = construct_square_map(total_square_len, all_symbols)
+    square_map = construct_square_map(mini_square_length, all_symbols)
 
     for loc, symbol in symbol_loc_map.items():
         insert_symbol(square_map, loc, symbol)
 
+    print_square_map(square_map)
     return square_map
 
+
+def print_square_map(square_map):
+    for row in range(square_map.side_len):
+        line = []
+        for loc, sym in square_map.items():
+            if loc.row == row:
+                line.append((loc, sym))
+        line.sort()
+        print(' '.join((str(e[1]) for e in line)))
 
 def main():
     square_map = parse_input_file('test_cases/squares/input_1.txt')
