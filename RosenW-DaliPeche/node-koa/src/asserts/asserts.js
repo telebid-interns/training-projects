73f4db21654fcf18de2076b79c38411321f1e29d
Rosen <ros.miryanov@gmail.com>
2018-09-10 17:54:01 +0300

    asserting asserts

--- RosenW-DaliPeche/node-koa/src/asserts/asserts.js
+++ RosenW-DaliPeche/node-koa/src/asserts/asserts.js
@@ -1,24 +1,31 @@
 const { AppError, PeerError, UserError } = require('./exceptions.js');
 
 const assert = (condition, msg, statusCode) => {
-  if (condition) {
-    return;
-  }
+  testIfConditionIsBoolean(condition, statusCode);
+  if (condition) return;
 
   throw new AppError(msg, statusCode);
 };
 
 const assertPeer = (condition, msg, statusCode) => {
+  testIfConditionIsBoolean(condition, statusCode);
   if (condition) return;
 
   throw new PeerError(msg, statusCode);
 };
 
 const assertUser = (condition, msg, statusCode) => {
+  testIfConditionIsBoolean(condition, statusCode);
   if (condition) return;
 
   throw new UserError(msg, statusCode);
 };
+
+const testIfConditionIsBoolean = (condition, statusCode) => {
+  if (typeof condition !== 'boolean') {
+    throw new AppError(`type of condition in assert with status code ${statusCode} should be boolean instead got ${typeof condition}`, 999);
+  }
+}
 
 module.exports = {
     assert,
