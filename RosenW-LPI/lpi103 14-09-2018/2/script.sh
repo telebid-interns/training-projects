cee42c9eb6bb4e8538882739d344f0dd3997fa85
Rosen <ros.miryanov@gmail.com>
2018-09-17 15:19:25 +0300

    solved second lpi problem

---
+++ RosenW-LPI/lpi103 14-09-2018/2/script.sh
@@ -0,0 +1,1 @@
+cat ../res/access.log | grep -Po '\[.*\] .* /.* - [0-9]+ - [0-9]+' | grep -Po ' /.*' | awk -F '".*"' '{print $1 $2}' | awk -F ' ' '{print $1 " " $4}' | awk -F '[\.\?].* ' '{print $1 " " $2}' | awk '$2>5000000' | sort -nr | awk -F ' ' '{print $1}' | uniq -c | sort -nr
