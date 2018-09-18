ab20330566083bd2dbfc85478a9415862e88ec05
Hristo Spasov <hristo_sp99@abv.bg>
2018-09-18 18:55:53 +0300

    Handle : in IP addresses and remove query string

--- hspasov-bash-parsing/big-response-times.sh
+++ hspasov-bash-parsing/big-response-times.sh
@@ -1,4 +1,4 @@
-CORRECT_LINE_PATTERN="^[^: ]+:[0-9]+ [a-zA-Z.0-9-]+ [^ ]+ [^ ]+ \[[^[]+\] \"[^\"]+\" [0-9]{3} [0-9]+ [0-9]+ \"[^\"]+\" \"[^\"]+\" - [0-9]+ - [0-9]+$"
+CORRECT_LINE_PATTERN="^[^: ]+:[0-9]+ [a-zA-Z.:0-9-]+ [^ ]+ [^ ]+ \[[^[]+\] \"[^\"]+\" [0-9]{3} [0-9]+ [0-9]+ \"[^\"]+\" \"[^\"]+\" - [0-9]+ - [0-9]+$"
 
 if [ ! -f $1 ]
   then
@@ -13,7 +13,7 @@ incorrect_lines_count=$(echo "$incorrect_lines" | wc -l)
 echo "$incorrect_lines" | sed "s/^/ERROR PARSING LINE /"
 echo
 
-grep -E "$CORRECT_LINE_PATTERN" $1 | awk -F\" '{ print $2 $7 }' | awk '{ print $2 " " $5 }' | awk '{ if($2 > 5000000) print $1 }' | sort | uniq -c | sort -nr
+grep -E "$CORRECT_LINE_PATTERN" $1 | awk -F\" '{ print $2 $7 }' | awk '{ print $2 " " $5 }' | awk '{ if($2 > 5000000) print $1 }' | awk -F? '{ print $1 }' | sort | uniq -c | sort -nr
 
 echo
 echo "Successfully parsed: $((all_lines_count - incorrect_lines_count))"
