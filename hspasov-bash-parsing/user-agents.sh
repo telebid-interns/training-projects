3022a8e02458744bcedbe96dbebe0bb1afdf42f4
Hristo Spasov <hristo_sp99@abv.bg>
2018-09-18 16:45:43 +0300

    Finish access agents bash parse task

--- hspasov-bash-parsing/user-agents.sh
+++ hspasov-bash-parsing/user-agents.sh
@@ -1,15 +1,20 @@
-# awk '{ print $14 }' ../../../Downloads/access.log | sort | uniq -c | sort
-if [ $# -eq 0 ]
+CORRECT_LINE_PATTERN="^[^: ]+:[0-9]+ [a-zA-Z.0-9-]+ [^ ]+ [^ ]+ \[[^[]+\] \"[^\"]+\" [0-9]{3} [0-9]+ [0-9]+ \"[^\"]+\" \"[^\"]+\" - [0-9]+ - [0-9]+$"
+
+if [ ! -f $1 ]
   then
     echo "Expected argument file"
     exit 1
 fi
 
-all_rows_count=$(cat $1 | wc -l)
-correct_rows_count=$(cat $1 | grep -E "^[^: ]+:[0-9]+ [a-zA-Z.0-9-]+ [^ ]+ [^ ]+ \[[^[]+\] \"[^\"]+\" [0-9]{3} [0-9]+ [0-9]+ \"[^\"]+\" \"[^\"]+\" - [0-9]+ - [0-9]+$" | wc -l)
+incorrect_lines=$(grep -Evn "$CORRECT_LINE_PATTERN" $1)
+all_lines_count=$(cat $1 | wc -l)
+incorrect_lines_count=$(echo "$incorrect_lines" | wc -l)
 
-echo $correct_rows_count
-echo $all_rows_count
-echo $((all_rows_count - correct_rows_count))
+echo "$incorrect_lines" | sed "s/^/ERROR PARSING LINE /"
+echo
 
-cat $1 | grep -E "^[^: ]+:[0-9]+ [a-zA-Z.0-9-]+ [^ ]+ [^ ]+ \[[^[]+\] \"[^\"]+\" [0-9]{3} [0-9]+ [0-9]+ \"[^\"]+\" \"[^\"]+\" - [0-9]+ - [0-9]+$" | cut -d"\"" -f6 | cut -d" " -f1 | sort | uniq -c | sort -nr
+grep -E "$CORRECT_LINE_PATTERN" $1 | cut -d"\"" -f6 | cut -d" " -f1 | sort | uniq -c | sort -nr
+
+echo
+echo "Successfully parsed: $((all_lines_count - incorrect_lines_count))"
+echo "Unsuccessfully parsed: $incorrect_lines_count"
