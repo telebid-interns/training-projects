2cd99ec6ba6b5a058ae10cd7b66caa98ed784b8f
Rosen <ros.miryanov@gmail.com>
2018-09-21 15:24:11 +0300

    made log validation fixed first script

--- RosenW-LPI/lpi103 14-09-2018/1/script.sh
+++ RosenW-LPI/lpi103 14-09-2018/1/script.sh
@@ -1,1 +1,13 @@
-cat ../res/access.log | grep -Po '"-" "([^\s"-]+)' | cut -c6- | sort -n | uniq -c | sort -nr
+#!/bin/bash
+allLines=`cat ../res/access.log | wc -l`
+lineCount=0
+while read p; do
+  if [[ $p =~ ^[a-Z0-9\.\-]+:[0-9]+" "[0-9\.]+" - - ["[0-9]+"/"[a-Z]+"/"[0-9\:]+" "[\+0-9]+"] "\"[^\"]+\"" "[0-9]+" "[0-9]+" "[0-9]+" "\"[^\"]+\"" "\"[^\"]+\"" - "[0-9]+" - "[0-9]+$ ]]; then
+    ((lineCount++))
+  fi
+done <../res/access.log
+
+cat ../res/access.log | grep -Po '^[A-z0-9\.\-\_]+:\d+ [0-9\.]+ - - [\[\]0-9A-z\/\:\s\+]+ "[^"]+" \d+ \d+ \d+ "[^"]+" "[^"]+" - \d+ - \d+$' | awk '{print $14}' | cut -d '"' -f2 | sort -n | uniq -c | sort -nr
+
+echo "Valid line count: $lineCount"
+echo "Invalid line count: $((allLines-lineCount))"
