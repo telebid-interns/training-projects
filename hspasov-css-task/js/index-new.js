ea5be89a4e8a53f9f157b1648253f1f0d6724ab1
Hristo Spasov <hristo_sp99@abv.bg>
2018-09-25 16:14:00 +0300

    Design improvements

--- hspasov-css-task/js/index-new.js
+++
@@ -1,3 +0,0 @@
-const contentContainer = document.getElementById('content-container');
-document.getElementById('add-content-btn').addEventListener('click', (e) => contentContainer.innerHTML += '<div class="container-item"><img class="container-image" src="./images/mountain.jpeg"></div>');
-document.getElementById('remove-content-btn').addEventListener('click', (e) => contentContainer.firstChild && contentContainer.removeChild(contentContainer.firstChild));
--- hspasov-css-task/js/index.js
+++ hspasov-css-task/js/index.js
@@ -1,3 +1,3 @@
-const contaner = document.getElementById('container');
-document.getElementById('add-content-btn').addEventListener('click', (e) => container.innerHTML += '<div class="container-item"><img class="container-image" src="./images/mountain.jpeg"></div>');
-document.getElementById('remove-content-btn').addEventListener('click', (e) => container.firstChild && container.removeChild(container.firstChild));
+const contentContainer = document.getElementById('content-container');
+document.getElementById('add-content-btn').addEventListener('click', (e) => contentContainer.innerHTML += '<div class="container-item"><img class="container-image" src="./images/mountain.jpeg"></div>');
+document.getElementById('remove-content-btn').addEventListener('click', (e) => contentContainer.firstChild && contentContainer.removeChild(contentContainer.firstChild));
