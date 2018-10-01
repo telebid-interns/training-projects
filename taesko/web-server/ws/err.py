6fa9638ccbde51e75bdaa7f4bf48a1afb592a1df
Antonio Todorov <taeskow@gmail.com>
2018-10-01 18:00:26 +0300

    Remove AppError exception from project.

--- taesko/web-server/ws/err.py
+++ taesko/web-server/ws/err.py
@@ -19,17 +19,5 @@ class SysError(BaseError):
     pass
 
 
-class AppError(BaseError):
-    default_msg='An internal server error occurred'
-
-    def __init__(self, *, msg=default_msg, code=None):
-        super().__init__(msg=msg, code=code)
-
-    @classmethod
-    def assert_(cls, condition, *, msg=default_msg, code=None):
-        super().assert_(condition, msg=msg, code=code)
-
-
 assert_peer = PeerError.assert_
 assert_sys = PeerError.assert_
-assert_app = AppError.assert_
