f6c6365b2bbbad2a6be1fa26db8ce89120148c8a
Antonio Todorov <taeskow@gmail.com>
2018-10-01 15:38:00 +0300

    Reorder classes/methods in server.py

--- taesko/web-server/ws/server.py
+++ taesko/web-server/ws/server.py
@@ -12,55 +12,6 @@ from ws.err import *
 error_log = logging.getLogger('error')
 
 
-class RequestReceiver:
-    buffer_size = 2048
-
-    def __init__(self, sock):
-        self.sock = sock
-        self.chunks = []
-        self.current_chunk = None
-        self.socket_broke = False
-
-    def __iter__(self):
-        return self
-
-    def __next__(self):
-        if self.current_chunk:
-            try:
-                return next(self.current_chunk)
-            except StopIteration:
-                pass
-        elif self.socket_broke:
-            raise StopIteration()
-
-        chunk = self.sock.recv(self.__class__.buffer_size)
-        error_log.debug('Read chunk %s', chunk)
-
-        if chunk == b'':
-            error_log.critical('Socket %s broke', self.sock)
-            self.socket_broke = True
-            raise StopIteration()
-            # raise ws.err.PeerError(code='BROKEN_SOCKET',
-            #                        msg='Client send 0 bytes through socket.')
-
-        self.chunks.append(chunk)
-        self.current_chunk = iter(chunk)
-
-        return next(self.current_chunk)
-
-    def recv_until_body(self):
-        last_four = collections.deque(maxlen=4)
-        for b in self:
-            last_four.append(b)
-
-            if bytes(last_four) == b'\r\n\r\n':
-                error_log.debug('Received request headers.')
-                break
-        else:
-            raise PeerError(msg='HTTP request has no trailing CR-LF-CR-LF',
-                            code='REQUEST_HAS_NO_END')
-
-
 class Server:
     ActiveWorker = collections.namedtuple('ActiveWorker', ['pid', 'created_on'])
 
@@ -73,6 +24,31 @@ class Server:
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.workers = collections.deque()
+
+    def __enter__(self):
+        error_log.debug('Binding server on %s:%s', self.host, self.port)
+        self.sock.bind((self.host, self.port))
+
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if not exc_type:
+            error_log.info("Closing server's leftover listening socket")
+            self.sock.close()
+            return False
+
+        error_log.exception('Unhandled error while listening.'
+                            'Shutting down...')
+        error_log.info("Closing server's listening socket")
+
+        self.sock.close()
+
+        if self.workers:
+            error_log.info('%d child process found. Waiting completion',
+                           len(self.workers))
+            os.wait()
+
+        return False
 
     def listen(self):
         error_log.debug('Listening...')
@@ -109,32 +85,57 @@ class Server:
                 self.workers = leftover_workers
                 error_log.info('%d active workers left.', len(self.workers))
 
-    def __enter__(self):
-        error_log.debug('Binding server on %s:%s', self.host, self.port)
-        self.sock.bind((self.host, self.port))
 
+class RequestReceiver:
+    buffer_size = 2048
+
+    def __init__(self, sock):
+        self.sock = sock
+        self.chunks = []
+        self.current_chunk = None
+        self.socket_broke = False
+
+    def __iter__(self):
         return self
 
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        if not exc_type:
-            error_log.info("Closing server's leftover listening socket")
-            self.sock.close()
-            return False
+    def __next__(self):
+        if self.current_chunk:
+            try:
+                return next(self.current_chunk)
+            except StopIteration:
+                pass
+        elif self.socket_broke:
+            raise StopIteration()
 
-        error_log.exception('Unhandled error while listening.'
-                            'Shutting down...')
-        error_log.info("Closing server's listening socket")
+        chunk = self.sock.recv(self.__class__.buffer_size)
+        error_log.debug('Read chunk %s', chunk)
 
-        self.sock.close()
+        if chunk == b'':
+            error_log.critical('Socket %s broke', self.sock)
+            self.socket_broke = True
+            raise StopIteration()
+            # raise ws.err.PeerError(code='BROKEN_SOCKET',
+            #                        msg='Client send 0 bytes through socket.')
 
-        if self.workers:
-            error_log.info('%d child process found. Waiting completion',
-                           len(self.workers))
-            os.wait()
+        self.chunks.append(chunk)
+        self.current_chunk = iter(chunk)
 
-        return False
+        return next(self.current_chunk)
+
+    def recv_until_body_depreciated(self):
+        last_four = collections.deque(maxlen=4)
+        for b in self:
+            last_four.append(b)
+
+            if bytes(last_four) == b'\r\n\r\n':
+                error_log.debug('Received request headers.')
+                break
+        else:
+            raise PeerError(msg='HTTP request has no trailing CR-LF-CR-LF',
+                            code='REQUEST_HAS_NO_END')
 
 
+# noinspection PyUnusedLocal
 def handle_connection(sock, address):
     request_receiver = RequestReceiver(sock)
 
