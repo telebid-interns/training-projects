6895ccb3aae85ebb4006bde18b77139be8ee9240
Antonio Todorov <taeskow@gmail.com>
2018-10-02 11:32:53 +0300

    Print module name and line number in logs for easier debugging.

--- taesko/web-server/ws/config.py
+++ taesko/web-server/ws/config.py
@@ -1,7 +1,6 @@
-import os
 import configparser
 import logging.config
-
+import os
 
 DEV_CONF = './conf.d/config.ini'
 
@@ -10,5 +9,6 @@ config = configparser.ConfigParser()
 config.read(os.environ.get('WS_CONFIG_FILE', DEV_CONF))
 
 
+# TODO have a basic config in case this function fails ?
 def configure_logging():
     logging.config.fileConfig(config['logging']['config_file'])
