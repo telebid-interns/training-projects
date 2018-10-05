3472899c49d1d035e20c43ce7a6e623fb6bcc8c1
Antonio Todorov <taeskow@gmail.com>
2018-10-05 11:20:09 +0300

    Check if config file exists and crash if it doesn't.

--- taesko/web-server/ws/config.py
+++ taesko/web-server/ws/config.py
@@ -8,17 +8,21 @@ DEV_CONF = './conf.d/config.ini'
 
 
 config = configparser.ConfigParser()
-config.read(os.environ.get('WS_CONFIG_FILE', DEV_CONF))
+config_file = os.environ.get('WS_CONFIG_FILE', DEV_CONF)
+assert_system(os.path.isfile(config_file), code='BAD_CONFIG_FILE',
+              msg='Config file {} does not exist '
+                  'or is a directory'.format(config_file))
+config.read(config_file)
 
 mode = config.get('settings', 'mode', fallback='production')
 production_mode = 'production'
 development_mode = 'dev'
 
-assert_sys(mode in (production_mode, development_mode),
-           msg='Invalid settings.mode variable. Must be one of {}'.format(
+assert_system(mode in (production_mode, development_mode),
+              msg='Invalid settings.mode variable. Must be one of {}'.format(
                ''.join((production_mode, development_mode))
            ),
-           code='CFG_BAD_SETTINGS_MODE')
+              code='CFG_BAD_SETTINGS_MODE')
 
 
 # TODO have a basic config in case this function fails ?
