11f59d4ec09b4f0c0d1371f9fc28f326640dd5bd
Antonio Todorov <taeskow@gmail.com>
2018-10-18 00:55:22 +0300

    Refactor benchmarking script to export better xlsx files.

--- taesko/web-server/bin/benchmark.py
+++ taesko/web-server/bin/benchmark.py
@@ -12,82 +12,128 @@ import sys
 import openpyxl
 import openpyxl.utils
 
-
-DEFAULT_COLUMN_WIDTH = 25
-
+DEFAULT_ROW_WIDTH = 40
+DEFAULT_CHAR_WIDTH = 2
+COLUMN_ORDER = (
+    'Server Software',
+    'Server Hostname',
+    'Server Port',
+    'Document Path',
+    'Document Length',
+    'Concurrency Level',
+    'Time taken for tests',
+    'Complete requests',
+    'Failed requests',
+    'Non-2xx responses',
+    'Total transferred',
+    'HTML transferred',
+    'Requests per second',
+    'Time per request',
+    'Time per request',
+    'Transfer rate'
+)
 
 ABResult = collections.namedtuple('ABResult', ['commit', 'fields', 'raw'])
 ABField = collections.namedtuple('ABField', ['field_text', 'value_text',
-                                             'name', 'value'])
+                                             'name', 'value', 'value_suffix'])
 
 
-class TimeValue(collections.namedtuple('TimeValue', ['raw', 'unit'])):
-    def __str__(self):
-        return str(self.raw)
+class TableExporter:
+    def __init__(self, sheet):
+        self.inserted_table_positions = []
+        self.sheet = sheet
+        self.column_widths = {}
+
+    def insert_table(self, table, column_names=None, position=(0, 0),
+                     table_name=None, expand_columns=True, expand_rows=True):
+        assert isinstance(table, collections.Sequence)
+        assert all(isinstance(r, collections.Sequence) for r in table)
+
+        if not table:
+            return 0, 0
+
+        row, col = position
+        if table_name:
+            r, _ = self.insert_row([table_name], (row, col),
+                                   expand_row=expand_rows,
+                                   expand_columns=False)
+            row += r
+        if column_names:
+            r, _ = self.insert_row(column_names, (row, col),
+                                   expand_row=expand_rows,
+                                   expand_columns=False)
+            row += r
+        for row_values in table:
+            r, _ = self.insert_row(row_values, (row, col),
+                                   expand_row=expand_rows,
+                                   expand_columns=expand_columns)
+            row += r
+
+
+        return len(table), len(table[0])
+
+    def insert_row(self, values, position=(0, 0), expand_row=True,
+                   expand_columns=True):
+        row, col = position
+        i = None
+
+        values = tuple(map(str, values))
+        for i, v in enumerate(values):
+            self.sheet.cell(row=row, column=col + i, value=v)
+
+        if expand_row:
+            line_c = max(v.count('\n') for v in values)
+            self.sheet.row_dimensions[row].height = DEFAULT_ROW_WIDTH * line_c
+        if expand_columns:
+            for i, val in enumerate(values):
+                letter = openpyxl.utils.get_column_letter(i + 1)
+                current_width = self.sheet.column_dimensions[letter].width or 0
+                required_width = len(val) * DEFAULT_CHAR_WIDTH
+                new_width = max(required_width, current_width)
+                self.sheet.column_dimensions[letter].width = new_width
+                self.column_widths[letter] = new_width
+
+        if i:
+            return 1, i + 1
+        else:
+            return 0, 0
+
+    def insert_table_from_dicts(self, dicts, column_names, position=(0, 0),
+                                table_name=None):
+        table = []
+        for dct in dicts:
+            sorted_ = sorted(dct.items,
+                             key=lambda entry: column_names.index(entry[0]))
+            table.append(tuple(entry[1] for entry in sorted_))
+
+        return self.insert_table(table=table, column_names=column_names,
+                                 position=position, table_name=table_name)
 
 
 class ABOutTypes(enum.Enum):
     default = 'default'
 
 
-def parse_ttt(text, value):
-    """ Time taken for tests."""
-    value_re = re.compile(r'([\d.]+) seconds')
-    match = value_re.match(value)
-
-    assert match
-
-    return ABField(field_text=text, value_text=value,
-                   name='ttt', value=TimeValue(raw=float(match.group(1)),
-                                               unit='seconds'))
-
-
-def parse_rps(text, value):
-    """ Requests per second."""
-    return parse_default_number(text, value)
-
-
-def parse_tpr(text, value):
-    """ Time per request."""
-    match = re.match(r'([\d.]+)', value)
-
-    assert match
-
-    return ABField(field_text=text, value_text=value,
-                   name='tpr', value=TimeValue(raw=float(match.group(1)),
-                                               unit='milliseconds'))
-
-
 def parse_default_number(text, value):
-    match = re.match(r'([\d.]+)', value)
+    match = re.match(r'([\d.]+)(.*)', value)
 
-    assert match
+    if not match:
+        raise ValueError('Value does not contain a number.')
 
     return ABField(field_text=text, value_text=value,
-                   name=text, value=float(match.group(1)))
+                   name=text, value=float(match.group(1)),
+                   value_suffix=match.group(2))
 
 
 def parse_default(text, value):
     return ABField(field_text=text, value_text=value,
-                   name=text, value=value)
-
-
-AB_FIELD_PARSERS = collections.OrderedDict(
-    (
-        ('Document Path', parse_default),
-        ('Concurrency Level', parse_default_number),
-        ('Complete requests', parse_default_number),
-        ('Failed requests', parse_default_number),
-        ('Requests per second', parse_rps),
-        ('Time per request', parse_tpr),
-        ('Time taken for tests', parse_ttt)
-    )
-)
+                   name=text, value=value, value_suffix='')
 
 
 def parse_ab_out(out, type_=ABOutTypes.default):
     field_regex = re.compile(r'\s*(.+?)\s*:\s*(.*)\s*')
-    fields = {}
+
+    result = []
 
     for i, line in enumerate(out.splitlines()):
         line = line.strip()
@@ -99,10 +145,19 @@ def parse_ab_out(out, type_=ABOutTypes.default):
         if not match:
             continue
 
-        fields[match.group(1).strip()] = match.group(2).strip()
+        field_text, value_text = match.group(1), match.group(2)
 
-    return [func(field, fields[field])
-            for field, func in AB_FIELD_PARSERS.items()]
+        if field_text not in COLUMN_ORDER:
+            continue
+
+        try:
+            ab_field = parse_default_number(field_text, value_text)
+        except ValueError:
+            ab_field = parse_default(field_text, value_text)
+
+        result.append(ab_field)
+
+    return result
 
 
 def run_test(ab_args):
@@ -110,7 +165,7 @@ def run_test(ab_args):
     print('Running AB with arguments: `{}`'
           .format(' '.join(ab_args)))
     out = subprocess.check_output(ab_args).decode('utf-8')
-    fields = {f.name: f for f in parse_ab_out(out)}
+    fields = parse_ab_out(out)
     git_out = subprocess.check_output(['git', 'log', '--oneline'],
                                       universal_newlines=True)
     commit = git_out.splitlines()[0]
@@ -140,9 +195,9 @@ def run_tests(host, port, *, request_numbers, concurrences, static_route,
         with open(raw_file_name, mode='w') as f:
             f.write(ab_result.raw)
 
-        json_file_name = os.path.join(benchmarks_dir, test_name + '.json')
-        with open(json_file_name, mode='w') as f:
-            json.dump(ab_result_to_dict(ab_result), f)
+        # json_file_name = os.path.join(benchmarks_dir, test_name + '.json')
+        # with open(json_file_name, mode='w') as f:
+        #     json.dump(ab_result_to_dict(ab_result), f)
 
         results.append(ab_result)
 
@@ -157,70 +212,33 @@ def run_tests(host, port, *, request_numbers, concurrences, static_route,
 
 
 def dump_ab_result_to_xlsx(ab_results, file_path):
-    assert len(set(ar.fields['Document Path'] for ar in ab_results)) == 1
+    assert len(set(ar.fields[0] for ar in ab_results)) == 1
 
     wb = openpyxl.Workbook()
     sheet = wb.active
     sheet.title = 'Benchmarks.'
     row, col = (1, 1)
+    te = TableExporter(sheet)
     table = []
-    column_names = tuple(AB_FIELD_PARSERS.keys())
+    column_names = None
 
     for ab_result in ab_results:
-        ab_fields = ab_result.fields.values()
-        ab_fields = sorted(
-            ab_fields,
-            key=lambda field: column_names.index(field.field_text)
-        )
-        table.append(tuple(f.value for f in ab_fields))
+        fields = ab_result.fields
+        if not column_names:
 
-    insert_table(sheet, (row, col), tuple(table), column_names=column_names)
+            column_names = ['{}\n({})'.format(field.field_text, field.value_suffix)
+                            for field in fields]
+        table_row = [field.value for field in fields]
+        table.append(table_row)
 
-    for i in range(1, len(column_names) + 1):
-        letter = openpyxl.utils.get_column_letter(i)
-        sheet.column_dimensions[letter].width = DEFAULT_COLUMN_WIDTH
+    te.insert_table(table=table,
+                    column_names=column_names,
+                    position=(row, col), table_name='Apache Benchmark results',
+                    expand_columns=True, expand_rows=True)
 
     wb.save(file_path)
 
     return
-
-
-def insert_table(sheet, position, table, *, column_names=None, table_name=None):
-    if not table:
-        return 0, 0
-
-    row, col = position
-    if table_name:
-        r, _ = insert_row(sheet, (row, col), [table_name])
-        row += r
-    if column_names:
-        r, _ = insert_row(sheet, (row, col), column_names)
-        row += r
-    for row_values in table:
-        r, _ = insert_row(sheet, (row, col), row_values)
-        row += r
-
-    return len(table), len(table[0])
-
-
-def insert_row(sheet, position, values):
-    row, col = position
-    i = None
-
-    for i, v in enumerate(values):
-        sheet.cell(row=row, column=col + i, value=str(v))
-
-    if i:
-        return 1, i + 1
-    else:
-        return 0, 0
-
-
-def ab_result_to_dict(ab_result):
-    dct = namedtuple_to_dict(ab_result)
-    dct['fields'] = {k: namedtuple_to_dict(v)
-                     for k, v in dct['fields'].items()}
-    return dct
 
 
 def namedtuple_to_dict(nt):
