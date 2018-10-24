bc214b24d0090156d26c58696d313bd590fb2bbf
Antonio Todorov <taeskow@gmail.com>
2018-10-25 00:30:23 +0300

    Compute per call values in grouped profiling.

--- taesko/web-server/bin/group_worker_profiling.sh
+++ taesko/web-server/bin/group_worker_profiling.sh
@@ -1,9 +1,16 @@
 #!/usr/bin/env bash
 awk '
-$2 ~ /^[0-9.]+$/ { cumtime[$6] += $4; tottime[$6] += $2; count[$6]++; calls[$6] += $1; }
+$2 ~ /^[0-9.]+$/ {
+    ct = "";
+    for (i = 6; i <= NF; i++) ct = ct $i " ";
+    cumtime[ct] += $4; tottime[ct] += $2; count[ct]++; calls[ct] += $1;
+}
 END {
     for (ct in cumtime) {
-        printf "%f %f %s %s\n", cumtime[ct]/count[ct], tottime[ct]/count[ct], calls[ct]/count[ct], ct;
+        ct_avg = cumtime[ct]/count[ct]
+        tott_avg = tottime[ct]/count[ct]
+        calls_avg = calls[ct]/count[ct]
+        printf "%f %f %s %f %f %s\n", ct_avg, tott_avg, calls_avg, (ct_avg)/(calls_avg), tott_avg/calls_avg, ct;
     }
 }' ${1:-./data/logs/profile.log} | sort -k ${3:-1} | tail -n ${2:-20}
 grep 'in .* seconds' ${1:-./data/logs/profile.log} | awk '{ sum += $8; count ++ } END { print "Average time spent profiling:", sum/count; }'
