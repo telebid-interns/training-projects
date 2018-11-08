508dbc844ff24b26673d94d87da98b137f23a14f
Rosen <rosen@arc-global.org>
2018-11-08 14:58:23 +0200

    printing result on stdout and current settings on stderr

--- RosenW-MonitoringScript/wp-mon-scr.php
+++ RosenW-MonitoringScript/wp-mon-scr.php
@@ -1,98 +1,49 @@
 <?php
-    class WordPressMonitor {
-        const CONFIG_PATH = '/etc/wordpress/';
-
-        function __construct($wp_path, $domain) {
-            $this->$wp_path = $wp_path;
-            $this->$domain = $domain;
-
-            $this->config_file = sprintf('%sconfig-%s.php', self::CONFIG_PATH, $domain);
-            assert_user(include $this->config_file, sprintf('Could not find %s', $this->config_file));
-
-            assert_user(defined('DB_HOST'), sprintf('DB_HOST not defined in %s', $this->config_file));
-            assert_user(defined('DB_NAME'), sprintf('DB_NAME not defined in %s', $this->config_file));
-            assert_user(defined('DB_PASSWORD'), sprintf('DB_PASSWORD not defined in %s', $this->config_file));
-            assert_user(defined('DB_USER'), sprintf('DB_USER not defined in %s', $this->config_file));
-
-            assert_user(isset($table_prefix), 'WordPress table_prefix not defined');
-
-            $this->table_prefix = $table_prefix;
-
-            $this->conn = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
-            assert_user(is_null($this->conn->connect_error), 'Connection to WP db failed: ' . $this->conn->connect_error);
-
-            $this->option_expected_dict = [
-                'users_can_register' => '0',
-                'default_comment_status' => 'closed'
-            ];
-
-            $this->option_actual_dict = [];
-        }
-
-        function start_test () {
-            $has_differences = false;
-
-            foreach ($this->option_expected_dict as $option) {
-                $stmt = $this->conn->prepare("SELECT option_value FROM wp_options WHERE option_name = ?");
-
-                if (!$stmt) {
-                    echo "Execute failed: (" . $this->conn->errno . ") " . $this->conn->error;
-                }
-
-                if ($stmt->execute(array($option))) {
-                    while ($row = $stmt->fetch()) {
-                        print_r($row);
-                    }
-                }
-
-                $stmt->close();
-
-                // assert_user(, 'WP version not supported by script');
-
-                // if (!$stmt->bind_param("s", $option)) {
-                //     echo "Binding parameters failed: (" . $stmt->errno . ") " . $stmt->error;
-                // }
-
-                // if (!$stmt->execute()) {
-                //     echo "Execute failed: (" . $stmt->errno . ") " . $stmt->error;
-                // }
-
-                // if (!($rows = $stmt->get_result())) {
-                //     echo "Getting result set failed: (" . $stmt->errno . ") " . $stmt->error;
-                // }
-            }
-        }
-    }
-
-    function assert_user ($condition, $msg) {
-        if (!$condition) {
-            throw new UserError($msg);
-        }
-    }
-
-    class UserError extends Exception {
-        public function __construct($message, $code = 0, Exception $previous = null) {
-            parent::__construct($message, $code, $previous);
-        }
+    function throw_usr_err ($msg) {
+        fwrite(STDOUT, sprintf("Error: %s" . PHP_EOL, $msg));
+        exit(1);
     }
 
     // Script Starts here
     if ($argv && $argv[0] && realpath($argv[0]) === __FILE__) {
-        // set_error_handler(function () { /* ignore notices/warnings */ });
-        try {
-            assert_user(count($argv) > 2, 
-                "
-                Please provide both wordpress installation path and domain name
+        count($argv) > 1 or throw_usr_err("\n\tPlease provide domain name as first argument\n\n\tTry 'php <script> <domain>'\n");
 
-                Try 'php <script> <wp_installation_path> <domain>'
-                "
-            );
-            $monitor = new WordPressMonitor($argv[1], $argv[2]);
-            $monitor->start_test();
-        } catch (UserError $e) {
-            // TODO stderr
-            echo $e->getMessage(), "\n";
-            exit;
+        $config_file = sprintf('/etc/wordpress/config-%s.php', $argv[1]);
+        (@include $config_file) or throw_usr_err (sprintf("Could not find %s", $config_file));
+
+        defined('DB_HOST') or throw_usr_err(sprintf("DB_HOST not defined in %s", $config_file));
+        defined('DB_NAME') or throw_usr_err(sprintf("DB_NAME not defined in %s", $config_file));
+        defined('DB_PASSWORD') or throw_usr_err(sprintf("DB_PASSWORD not defined in %s", $config_file));
+        defined('DB_USER') or throw_usr_err(sprintf("DB_USER not defined in %s", $config_file));
+
+        isset($table_prefix) or $table_prefix = 'wp_';
+
+        $conn = @new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+        is_null($conn->connect_error) or throw_usr_err(sprintf("Could not connect to Database check credentials in %s", $config_file));
+
+        $option_expected_dict = [
+            'users_can_register' => '0',
+            'default_comment_status' => 'closed'
+        ];
+
+        $option_actual_dict = [];
+
+        $has_differences = false;
+
+        foreach ($option_expected_dict as $option => $value) {
+            $result = $conn->query(sprintf("SELECT option_value FROM %soptions WHERE option_name = '%s'", $table_prefix, $option)) or throw_usr_err(sprintf("Could not find option_name value in table %soptions, please check if %s is your wordpress table prefix", $table_prefix, $table_prefix));
+
+            while ($row = mysqli_fetch_assoc($result)) {
+                fwrite(STDERR, sprintf("%s: %s" . PHP_EOL, $option, $row['option_value']));
+                if ($value !== $row['option_value']) $has_differences = true;
+            }
         }
+
+        if ($has_differences) {
+            fwrite(STDOUT, "0");
+            exit(0);
+        }
+
+        fwrite(STDOUT, "1");
     }
 ?>
