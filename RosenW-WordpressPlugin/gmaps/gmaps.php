97bad3f1af3163ff641281191124210c314bc2bf
Rosen <rosen@arc-global.org>
2018-11-09 16:06:53 +0200

    GMaps plugin v1

--- RosenW-WordpressPlugin/gmaps/gmaps.php
+++ RosenW-WordpressPlugin/gmaps/gmaps.php
@@ -1,48 +1,79 @@
 <?php
-  /**
-   * Plugin Name: GMaps
-   * Description: Replaces [GMaps-map] with an actual google map
-   * Version: 0.2
-   * Author: Rosen
-   */
+    /**
+    * Plugin Name: GMaps
+    * Description: Replaces [GMaps-map] with an actual google map
+    * Version: 1.0
+    * Author: Rosen
+    */
 
-  /*
-  * Use example:
-  *
-  * [GMaps-map markers="(18, 16);(12, 33)" zoom="1" center="(52, 162)" width="600" height="600" key="API_KEY_HERE"]
-  */
+    /*
+    * Use example:
+    *
+    * [GMaps-map id="third" markers="(15, 50);(17.44, 9.5)" center="(15, 50)" zoom="5" width="600" height="600" units="px" key="AIzaSyDrLczSe8MEo3k5FAwo1Kc3fkn26NB-VUQ"]
+    */
 
-  defined('ABSPATH') or die('ABSPATH not defined');
+    defined('ABSPATH') or die('ABSPATH not defined');
 
-  add_shortcode('GMaps-map', 'add_map');
-  add_action('wp_enqueue_scripts', 'enqueue_scripts');
+    add_shortcode('GMaps-map', 'add_map');
+    add_action('wp_enqueue_scripts', 'enqueue_scripts');
 
-  function enqueue_scripts () {
-    wp_enqueue_script( 'init_maps', plugin_dir_url( __FILE__ ) . '/js/init-maps.js', array(), null, true);
-  }
-
-  function add_map ($atts) {
-    try {
-      if (array_key_exists('key', $atts) !== true) {
-        throw new InvalidArgumentException("Shortcode must have an API key parameter");
-      }
-
-      if (isset($GLOBALS['key']) && $GLOBALS['key'] !== $atts['key']) {
-        throw new Exception("Shortcode must have an API key parameter"); 
-      }
-
-      $GLOBALS['key'] = $atts['key'];
-
-      wp_enqueue_script( 'google_api_script', 'https://maps.googleapis.com/maps/api/js?key=' . $GLOBALS['key'] . '&callback=initMaps', array(), null, true);
-
-      return '<div id="' . $atts['id'] . '" class="gmaps-map" style="width: ' . $atts['width'] . 'px; height: ' . $atts['height'] . 'px" data-markers="' . $atts['markers'] . '" data-zoom="' . $atts['zoom'] . '" data-center="' . $atts['center'] . '"></div>';
-    } catch (Exception $e) {
-
-      if (WP_DEBUG === true) {
-        error_log(print_r($e->getMessage(), true));
-      }
-
-      return '<div>There was an error in shortcode [GMaps-map]</div>';
+    function enqueue_scripts () {
+        wp_enqueue_script( 'init_maps', plugin_dir_url( __FILE__ ) . '/js/init-maps.js', array(), null, true);
     }
-  }
+
+    function add_map ($atts) {
+        try {
+            assert_user(is_array($atts), "Shortcode doesn't contain any arguments");
+            assert_user(array_key_exists('key', $atts), "API key must be specified");
+            assert_user(array_key_exists('width', $atts), "Map width must be specified");
+            assert_user(array_key_exists('height', $atts), "Map height must be specified");
+            assert_user(array_key_exists('id', $atts), "DOM id must be specified");
+
+            if (!array_key_exists('units', $atts)) {
+                $atts['units'] = 'px';
+            }
+
+            assert_user(is_numeric($atts['width']), "Width must be a number");
+            assert_user(is_numeric($atts['height']), "Height must be a number");
+
+            assert_user(!isset($GLOBALS['key']) || $GLOBALS['key'] === $atts['key'], "Cannot use multiple API keys");
+
+            $GLOBALS['key'] = $atts['key'];
+
+            // unique name 'google_api_script' prevents wp_enqueue_script() from enqueuing script multiple times
+            wp_enqueue_script( 'google_api_script', sprintf('https://maps.googleapis.com/maps/api/js?key=%s&callback=initMaps', $GLOBALS['key']), array(), null, true);
+
+            return build_div($atts);
+
+        } catch (Exception $e) {
+            if (WP_DEBUG === true) {
+                error_log(print_r($e->getMessage(), true));
+            }
+
+            return '<div>There was an error in shortcode [GMaps-map]</div>';
+        }
+    }
+
+    function build_div ($atts) {
+        $div = sprintf('<div id="%s" class="gmaps-map" style="width: %s%s; height: %s%s" ', $atts['id'], $atts['width'], $atts['units'], $atts['height'], $atts['units']);
+        foreach ($atts as $key => $value) {
+            if (in_array($key, ["center", "zoom", "markers", "units"])) {
+                $div .= sprintf('data-%s="%s" ', $key, $value); 
+            }
+        }
+        $div .= '></div>';
+        return $div;
+    }
+
+    function assert_user ($condition, $msg) {
+        if (!$condition) {
+            throw new UserError($msg);
+        }
+    }
+
+    class UserError extends Exception {
+        public function __construct($message, $code = 0, Exception $previous = null) {
+            parent::__construct($message, $code, $previous);
+        }
+    }
 ?>
--- RosenW-WordpressPlugin/gmaps/js/init-maps.js
+++ RosenW-WordpressPlugin/gmaps/js/init-maps.js
@@ -1,38 +1,58 @@
 function initMaps () {
   gmaps = [...document.getElementsByClassName("gmaps-map")];
   gmaps.map((gmap) => {
-    const markers = [];
-
-    const center = { lat: 0, lng: 0 };
-
-    if (gmap.dataset.zoom == null) {
+    try {
+      // Default values
+      const center = { lat: 0, lng: 0 };
       let zoom = 1;
-    }
 
-    centerTokens = gmap.dataset.center.split(', ');
-    center.lat = Number(centerTokens[0].substr(1));
-    center.lng = Number(centerTokens[1].substr(0, centerTokens[1].length - 1));
-
-    const currentMap = new google.maps.Map(gmap, {
-      zoom: Number(gmap.dataset.zoom), 
-      center
-    });
-
-    if (gmap.dataset.markers == null) {
-      return;
-    }
-
-    for (const pair of gmap.dataset.markers.split(';')) {
-      const pairTokens = pair.split(', ');
-      const position = {
-        lat: Number(pairTokens[0].substr(1)),
-        lng: Number(pairTokens[1].substr(0, pairTokens[1].length - 1))
+      if (typeof gmap.dataset.zoom === 'string') {
+        assert(Number(gmap.dataset.zoom));
+        zoom = Number(gmap.dataset.zoom);
       }
 
-      new google.maps.Marker({
-        position,
-        map: currentMap
+      if (typeof gmap.dataset.center === 'string') {
+        centerTokens = gmap.dataset.center.split(', ');
+        center.lat = Number(centerTokens[0].substr(1));
+        center.lng = Number(centerTokens[1].substr(0, centerTokens[1].length - 1));
+        assert(Number(center.lat));
+        assert(Number(center.lng));
+      }
+
+      const map = new google.maps.Map(gmap, {
+        zoom, 
+        center
       });
+
+      if (typeof gmap.dataset.markers === 'string') {
+        for (const pair of gmap.dataset.markers.split(';')) {
+          const pairTokens = pair.split(', ');
+          const position = {
+            lat: Number(pairTokens[0].substr(1)),
+            lng: Number(pairTokens[1].substr(0, pairTokens[1].length - 1))
+          }
+
+          assert(Number(position.lat));
+          assert(Number(position.lng));
+
+          new google.maps.Marker({
+            position,
+            map
+          });
+        }
+      }
+    } catch (err) {
+      gmap.innerHTML = 'There was a problem showing the map.' 
     }
   });
+}
+
+function isObject (obj) {
+  return typeof obj === "object" && obj !== null;
+}
+
+function assert (condition) {
+  if (!condition) {
+    throw new Error();
+  }
 }
