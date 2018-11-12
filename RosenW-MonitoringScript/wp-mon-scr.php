2498c5e2a28c057645c7a29275f76acc095a5cd2
Rosen <rosen@arc-global.org>
2018-11-12 11:51:28 +0200

    added script that calls main script with all configs in etc/wp

--- RosenW-MonitoringScript/wp-mon-scr.php
+++ RosenW-MonitoringScript/wp-mon-scr.php
@@ -1,49 +1,15 @@
 <?php
-    function throw_usr_err ($msg) {
-        fwrite(STDOUT, sprintf("Error: %s" . PHP_EOL, $msg));
-        exit(1);
-    }
+    const CONFIG_PATH = "/etc/wordpress/";
 
     // Script Starts here
     if ($argv && $argv[0] && realpath($argv[0]) === __FILE__) {
-        count($argv) > 1 or throw_usr_err("\n\tPlease provide domain name as first argument\n\n\tTry 'php <script> <domain>'\n");
-
-        $config_file = sprintf('/etc/wordpress/config-%s.php', $argv[1]);
-        (@include $config_file) or throw_usr_err (sprintf("Could not find %s", $config_file));
-
-        defined('DB_HOST') or throw_usr_err(sprintf("DB_HOST not defined in %s", $config_file));
-        defined('DB_NAME') or throw_usr_err(sprintf("DB_NAME not defined in %s", $config_file));
-        defined('DB_PASSWORD') or throw_usr_err(sprintf("DB_PASSWORD not defined in %s", $config_file));
-        defined('DB_USER') or throw_usr_err(sprintf("DB_USER not defined in %s", $config_file));
-
-        isset($table_prefix) or $table_prefix = 'wp_';
-
-        $conn = @new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
-        is_null($conn->connect_error) or throw_usr_err(sprintf("Could not connect to Database", $config_file));
-
-        $option_expected_dict = [
-            'users_can_register' => '0',
-            'default_comment_status' => 'closed'
-        ];
-
-        $option_actual_dict = [];
-
-        $has_differences = false;
-
-        foreach ($option_expected_dict as $option => $value) {
-            $result = $conn->query(sprintf("SELECT option_value FROM %soptions WHERE option_name = '%s'", $table_prefix, $option)) or throw_usr_err(sprintf("Could not find option_name value in table %soptions, please check if %s is your wordpress table prefix", $table_prefix, $table_prefix));
-
-            while ($row = mysqli_fetch_assoc($result)) {
-                fwrite(STDERR, sprintf("%s: %s" . PHP_EOL, $option, $row['option_value']));
-                if ($value !== $row['option_value']) $has_differences = true;
+        $files = scandir(CONFIG_PATH);
+        foreach ($files as $file) {
+            if (substr($file, -4) === '.php' && strpos($file, "config-") === 0) {
+                $site = substr($file, 7, -4);
+                fwrite(STDOUT, PHP_EOL . sprintf("Result for %s", $site) . PHP_EOL);
+                fwrite(STDOUT, exec(sprintf("php ./read-config.php %s", $site)) . PHP_EOL);
             }
         }
-
-        if ($has_differences) {
-            fwrite(STDOUT, "0");
-            exit(0);
-        }
-
-        fwrite(STDOUT, "1");
     }
 ?>
