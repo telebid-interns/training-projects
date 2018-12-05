5ab829c221d4bb13fb8ccf73b2934bdba01c3f81
Rosen <rosen@arc-global.org>
2018-12-05 14:09:24 +0200

    monitoring script outs monjson format

--- RosenW-MonitoringScript/read-config.php
+++ RosenW-MonitoringScript/read-config.php
@@ -4,6 +4,50 @@
         exit(1);
     }
 
+    function generateJson ($values) {
+        $arr = [
+            'version' => '3.0',
+            'update_interval' => 60,
+            'applications' => [
+                'wp-settings-mon' => [
+                    'name' => 'WordPress Settings Monitoring',
+                    'items' => [
+                        'registration_allowed' => [
+                            'name' => 'WP Registration',
+                            'type' => 'bool',
+                            'value' => $values['users_can_register'],
+                            'timestamp' => time(),
+                            'triggers' => [
+                                'trig1' => [
+                                    'descr' => 'WordPress Registration allowed',
+                                    'prior' => 'warn',
+                                    'range' => [0, 0],
+                                    'resol' => 'Turn off user registration from the  admin panel or change in database table "<wp-prefix>options" set "option_value" to 0 where "option_name" is "users_can_register"'
+                                ],
+                            ]
+                        ],
+                        'comments_allowed' => [
+                            'name' => 'WP Comments',
+                            'type' => 'text',
+                            'value' => $values['default_comment_status'],
+                            'timestamp' => time(),
+                            'triggers' => [
+                                'trig1' => [
+                                    'descr' => 'WordPress Comments allowed',
+                                    'prior' => 'warn',
+                                    'match' => '^closed$',
+                                    'resol' => 'Turn off comments from the admin panel or change in database table "<wp-prefix>options" set "option_value" to "closed" where "option_name" is "default_comment_status"'
+                                ],
+                            ]
+                        ],
+                    ]
+                ]
+            ]
+        ];
+
+        return json_encode($arr);
+    }
+
     // Script Starts here
     if ($argv && $argv[0] && realpath($argv[0]) === __FILE__) {
         count($argv) > 1 or throw_usr_err("\n\tPlease provide domain name as first argument\n\n\tTry 'php <script> <domain>'\n");
@@ -21,29 +65,22 @@
         $conn = @new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
         is_null($conn->connect_error) or throw_usr_err(sprintf("Could not connect to Database", $config_file));
 
-        $option_expected_dict = [
-            'users_can_register' => '0',
-            'default_comment_status' => 'closed'
-        ];
+        $options = [ 'users_can_register', 'default_comment_status' ];
 
-        $option_actual_dict = [];
+        $values = [];
 
         $has_differences = false;
 
-        foreach ($option_expected_dict as $option => $value) {
-            $result = $conn->query(sprintf("SELECT option_value FROM %soptions WHERE option_name = '%s'", $table_prefix, $option)) or throw_usr_err(sprintf("Could not find option_name value in table %soptions, please check if %s is your wordpress table prefix", $table_prefix, $table_prefix));
+        foreach ($options as $opt) {
+            $result = $conn->query(sprintf("SELECT option_value FROM %soptions WHERE option_name = '%s'", $table_prefix, $opt)) or throw_usr_err(sprintf("Could not find option_name value in table %soptions, please check if %s is your wordpress table prefix", $table_prefix, $table_prefix));
 
             while ($row = mysqli_fetch_assoc($result)) {
-                fwrite(STDERR, sprintf("%s: %s" . PHP_EOL, $option, $row['option_value']));
-                if ($value !== $row['option_value']) $has_differences = true;
+                // fwrite(STDERR, sprintf("%s: %s" . PHP_EOL, $opt, $row['option_value']));
+                $values[$opt] = $row['option_value'];
             }
         }
 
-        if ($has_differences) {
-            fwrite(STDOUT, "0");
-            exit(0);
-        }
-
-        fwrite(STDOUT, "1");
+        fwrite(STDOUT, generateJson($values));
     }
 ?>
+
