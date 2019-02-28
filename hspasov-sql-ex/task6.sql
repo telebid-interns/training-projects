d666426512b3645e738544a81496db46c2b60ae0
Hristo Spasov <hristo_sp99@abv.bg>
2019-02-28 23:20:10 +0200

    Fix task6

--- hspasov-sql-ex/task6.sql
+++ hspasov-sql-ex/task6.sql
@@ -2,8 +2,7 @@
 
 -- For each maker producing laptops with a hard drive capacity of 10 Gb or higher, find the speed of such laptops. Result set: maker, speed.
 
--- not right:
-SELECT maker, speed
+SELECT DISTINCT maker, speed
 FROM Laptop
 JOIN Product
 ON Laptop.model = Product.model
